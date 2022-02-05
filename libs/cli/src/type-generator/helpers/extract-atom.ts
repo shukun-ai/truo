50c0fd5de4760836272c8d408e6f7a7a2583db1d
Chase Wu <minowu@foxmail.com>
2022-02-05 17:40:52 +0800

    feat: new type-generator tool for cli

---
+++ libs/cli/src/type-generator/helpers/extract-atom.ts
@@ -0,0 +1,91 @@
+import {
+  MetadataElectron,
+  MetadataFieldType,
+  MetadataSchema,
+} from '@shukun/schema';
+import { toPascalCase } from 'js-convert-case';
+import {
+  AttachmentSchema,
+  DateTimeIsoString,
+  IDString,
+} from './extract-shared';
+
+export function extractAtom(atom: MetadataSchema): string {
+  let text = '';
+
+  text += `export interface ${toPascalCase(atom.name)}Model {`;
+
+  atom.electrons.forEach((electron) => {
+    text += extractElectron(electron);
+  });
+
+  text += `};`;
+
+  return text;
+}
+
+function extractElectron(electron: MetadataElectron): string {
+  let text = '';
+
+  const equalMark = electron.isRequired ? ':' : '?:';
+
+  text += `${electron.name}${equalMark} ${parseElectronFieldType(electron)}; `;
+
+  return text;
+}
+
+function parseElectronFieldType(electron: MetadataElectron): string {
+  let text = '';
+
+  switch (electron.fieldType) {
+    case MetadataFieldType.Text:
+    case MetadataFieldType.NameText:
+    case MetadataFieldType.LargeText:
+    case MetadataFieldType.Password:
+      text = 'string';
+      break;
+    case MetadataFieldType.DateTime:
+      text = `${DateTimeIsoString}`;
+      break;
+    case MetadataFieldType.ManyToOne:
+    case MetadataFieldType.Owner:
+      text = `${IDString}`;
+      break;
+    case MetadataFieldType.ManyToMany:
+    case MetadataFieldType.Role:
+      text = `${IDString}[]`;
+      break;
+    case MetadataFieldType.Integer:
+    case MetadataFieldType.Float:
+    case MetadataFieldType.Currency:
+      text = 'number';
+      break;
+    case MetadataFieldType.Boolean:
+      text = 'boolean';
+      break;
+    case MetadataFieldType.Attachment:
+      text = `${AttachmentSchema}[]`;
+      break;
+    case MetadataFieldType.SingleSelect:
+      text = `${listElectronOptionKey(electron)}`;
+      break;
+    case MetadataFieldType.MultiSelect:
+      text = `(${listElectronOptionKey(electron)})[]`;
+      break;
+    case MetadataFieldType.Mixed:
+      text = 'unknown';
+      break;
+    default:
+      text = 'unknown';
+  }
+
+  return text;
+}
+
+export function listElectronOptionKey(electron: MetadataElectron): string {
+  if (!electron.options) {
+    return 'undefined';
+  }
+
+  return electron.options.map((option) => `"${option.key}"`).join('|');
+}
