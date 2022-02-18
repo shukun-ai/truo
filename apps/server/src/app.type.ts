d9c80d04f4a517237a6680bf34511994f403480a
Chase Wu <minowu@foxmail.com>
2022-02-18 20:08:10 +0800

    feat: separate mongo connection service from source util service.

--- apps/server/src/app.type.ts
+++ apps/server/src/app.type.ts
@@ -4,4 +4,4 @@ export interface SourceServiceCreateDto {
   [keyName: string]: unknown;
 }
 
-export type JsonModel<Model> = { _id?: IDString } & Model;
+export type JsonModel<Model> = { _id: IDString } & Model;
