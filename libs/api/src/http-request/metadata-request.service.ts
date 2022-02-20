ad4452cba99c1907cf192d5840527756c5a1e936
Chase Wu <minowu@foxmail.com>
2022-02-20 19:14:28 +0800

    feat: refactor restful-request and metadata-request.

--- libs/api/src/http-request/metadata-request.service.ts
+++ libs/api/src/http-request/metadata-request.service.ts
@@ -1,9 +1,10 @@
-import { MetadataSchema, RoleResourceType } from '@shukun/schema';
+import { MetadataElectron, MetadataSchema } from '@shukun/schema';
 import { AxiosResponse } from 'axios';
 
 import { HttpRequestService } from './http-request.service';
 
 import { RestfulRequestService } from './restful-request.service';
+import { IDString } from './shared-types';
 import {
   UnknownSourceModel,
   ApiResponseData,
@@ -13,56 +14,77 @@ import {
 export class MetadataRequestService<Model = UnknownSourceModel> {
   metadata: MetadataSchema;
   request: RestfulRequestService<Model>;
+  globalSelect: Record<keyof Model, true>;
 
   constructor(
     private readonly httpRequestService: HttpRequestService,
     metadata: MetadataSchema,
-    options?: { globalSelect?: string[] },
   ) {
-    const urlPath = metadata.name;
-    const globalSelect =
-      options?.globalSelect ??
-      metadata.electrons
-        .map((item) => item.name)
-        .concat('_id', 'updatedAt', 'createdAt');
-
+    const atomName = metadata.name;
     this.metadata = metadata;
 
     this.request = new RestfulRequestService<Model>(this.httpRequestService, {
-      resourceType: RoleResourceType.Source,
-      urlPath,
-      globalSelect: globalSelect as any,
+      atomName,
     });
+
+    this.globalSelect = this.convertGlobalSelect(metadata.electrons);
+  }
+
+  protected convertGlobalSelect(
+    electrons: MetadataElectron[],
+  ): Record<keyof Model, true> {
+    const selectKeys: string[] = electrons
+      .map((item) => item.name)
+      .concat('_id', 'updatedAt', 'createdAt');
+
+    const initialValue = {} as Record<keyof Model, true>;
+
+    const select = selectKeys.reduce((previous, current) => {
+      previous[current as keyof Model] = true;
+      return previous;
+    }, initialValue);
+
+    return select;
   }
 
   public async findMany(
     params?: QueryParams,
   ): Promise<AxiosResponse<ApiResponseData<Model[]>>> {
-    return this.request.findMany(params);
+    return this.request.findMany(params || {}, this.globalSelect);
   }
 
   public async findOne(
     params?: QueryParams,
   ): Promise<AxiosResponse<ApiResponseData<Model>>> {
-    return this.request.findOne(params);
+    return this.request.findOne(params || {}, this.globalSelect);
   }
 
-  public async createOne(data: Record<string, any> & Partial<Model>) {
+  public async createOne(data: Partial<Model>): Promise<
+    AxiosResponse<
+      ApiResponseData<{
+        _id: IDString;
+      }>
+    >
+  > {
     return this.request.createOne(data);
   }
 
-  public async createAndFindOne(data: Record<string, any> & Partial<Model>) {
-    return this.request.createAndFindOne(data);
+  public async createAndFindOne(
+    data: Partial<Model>,
+  ): Promise<AxiosResponse<ApiResponseData<Model>>> {
+    return this.request.createAndFindOne(data, this.globalSelect);
   }
 
   public async updateOne(
     id: string,
-    data: Record<string, any> & Partial<Model>,
-  ) {
+    data: Partial<Model>,
+  ): Promise<AxiosResponse<ApiResponseData<null>>> {
     return this.request.updateOne(id, data);
   }
 
-  public async removeOne(id: string) {
+  public async removeOne(
+    id: string,
+  ): Promise<AxiosResponse<ApiResponseData<null>>> {
     return this.request.removeOne(id);
   }
 }
