49bd92c62d25aa805cb1b73b98083e7fe2133a5e
Chase Wu <minowu@foxmail.com>
2022-08-30 21:18:59 +0800

    feat: use latest apis standard.

--- apps/server/src/identity/utils/security.utils.ts
+++ apps/server/src/identity/utils/security.utils.ts
@@ -2,6 +2,8 @@ import { BadRequestException } from '@nestjs/common';
 import { AccessControl } from 'accesscontrol';
 import { Request } from 'express';
 
+import { ResourceNodes } from '../../api/authorization/authorization.interface';
+
 import { AccessActionRange, AccessActionType, UrlNodes } from '../interfaces';
 
 export function parseToken(authorization: string | undefined): string | null {
@@ -61,8 +63,49 @@ export const createAccessPermission = (
 };
 
 export function getAccessActionType(
-  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  resourceNodes: ResourceNodes,
 ): AccessActionType {
+  if (!resourceNodes.resourceFunction) {
+    return getLegendAccessActionType(resourceNodes.method);
+  } else {
+    return getAllPostAccessActionType(
+      resourceNodes.method,
+      resourceNodes.resourceFunction,
+    );
+  }
+}
+
+export function getAllPostAccessActionType(
+  method: ResourceNodes['method'],
+  resourceFunction: NonNullable<ResourceNodes['resourceFunction']>,
+) {
+  if (method === 'POST') {
+    if (['metadata', 'query'].includes(resourceFunction)) {
+      return AccessActionType.Read;
+    }
+    if (['create'].includes(resourceFunction)) {
+      return AccessActionType.Create;
+    }
+
+    if (
+      ['update', 'add-to-many', 'remove-from-many', 'increase'].includes(
+        resourceFunction,
+      )
+    ) {
+      return AccessActionType.Update;
+    }
+
+    if (['delete'].includes(resourceFunction)) {
+      return AccessActionType.Delete;
+    }
+  }
+
+  throw new BadRequestException(
+    'method 和 resourceFunction 均不匹配，无法解析权限。',
+  );
+}
+
+export function getLegendAccessActionType(method: ResourceNodes['method']) {
   switch (method) {
     case 'GET':
       return AccessActionType.Read;
