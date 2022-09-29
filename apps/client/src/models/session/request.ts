426316abfa2602236df6a239e931bbd06497e881
Chase Wu <minowu@foxmail.com>
2022-09-29 09:43:55 +0800

    feat: implement password in jwt_encrypt.

--- apps/client/src/models/session/request.ts
+++ apps/client/src/models/session/request.ts
@@ -11,12 +11,28 @@ export interface SignInData {
   password: string;
 }
 
+export interface SignInDataWithEncrypt {
+  orgName: string;
+  username: string;
+  encryptPassword: string;
+}
+
 export async function signIn(data: SignInData) {
+  const signInDataWithEncrypt: SignInDataWithEncrypt = {
+    orgName: data.orgName,
+    username: data.username,
+    encryptPassword: encryptPassword(data.password),
+  };
+
   const response = await httpRequestService
     .createAxios()
     .post<ApiResponseData<AuthApiModel>>(
-      `${RoleResourceType.Public}/${data.orgName}/authentication/jwt`,
-      data,
+      `${RoleResourceType.Public}/${data.orgName}/authentication/jwt_encrypt`,
+      signInDataWithEncrypt,
     );
   return response;
+}
+
+function encryptPassword(password: string) {
+  return password;
 }
