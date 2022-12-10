18df38f9d8e807da5e38b6fb74130effa48a1c6f
Chase Wu <minowu@foxmail.com>
2022-12-10 13:45:22 +0800

    fix: show internal filter.

--- apps/client/src/pages/view/components/fields/date-time/DateTimeFilter.tsx
+++ apps/client/src/pages/view/components/fields/date-time/DateTimeFilter.tsx
@@ -57,7 +57,7 @@ export const DateTimeFilterInput: FunctionComponent<
         };
         onChange(value);
       } else {
-        onChange(null);
+        onChange(undefined);
       }
     },
     [onChange],
