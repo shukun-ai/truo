9076aa4600d4560696a74c45476cb4b3cc7dff0e
Chase Wu <minowu@foxmail.com>
2022-12-15 15:09:52 +0800

    feat: add last-or-throw.

---
+++ apps/server/src/compiler/events/compile-last-or-throw.ts
@@ -0,0 +1,26 @@
+import { FlowEventLastOrThrow } from '@shukun/schema';
+
+export async function compileLastOrThrowEvent(
+  event: FlowEventLastOrThrow,
+): Promise<string> {
+  return `
+        async function main($, $$, $$$){
+            if (!Array.isArray($.input)) {
+              throw new $$$.IsNotArrayException('The input is not a array.');
+            }
+
+            if ($.input.length === 0) {
+              throw new $$$.IsEmptyArrayException('The input is a empty array.');
+            }
+
+            const output = $.input[$.input.length - 1];
+
+            return {
+              ...$,
+              next: "${event.next}",
+              output
+            }
+        };
+        exports.default=main;
+    `;
+}
