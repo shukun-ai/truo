e28f2d1276dca4e2bd82108acce727b0f9bea4a2
Chase Wu <minowu@foxmail.com>
2022-12-17 22:45:08 +0800

    feat: hide console.

--- apps/editor/src/pages/flow/nodes/event-node-form.tsx
+++ apps/editor/src/pages/flow/nodes/event-node-form.tsx
@@ -10,11 +10,11 @@ export const EventNodeForm: FunctionComponent<EventNodeFormProps> = ({
   children,
 }) => {
   const onFinish = (values: any) => {
-    console.log('Success:', values);
+    // console.log('Success:', values);
   };
 
   const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
+    // console.log('Failed:', errorInfo);
   };
 
   return (
