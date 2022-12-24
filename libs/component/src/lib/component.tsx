cd55f658d43f66bd113bfc21893e06d44db1cf49
Chase Wu <minowu@foxmail.com>
2022-12-24 11:24:39 +0800

    feat: new component lib.

---
+++ libs/component/src/lib/component.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ComponentProps {}
+
+export function Component(props: ComponentProps) {
+  return (
+    <div>
+      <h1>Welcome to Component!</h1>
+    </div>
+  );
+}
+
+export default Component;
