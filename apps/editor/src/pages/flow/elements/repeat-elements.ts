0e197b2aeaa327418a4933c8ff0d5da4ec374b64
Chase Wu <minowu@foxmail.com>
2022-12-25 13:46:56 +0800

    feat: add more elements.

---
+++ apps/editor/src/pages/flow/elements/repeat-elements.ts
@@ -0,0 +1,26 @@
+import { FlowEventRepeat } from '@shukun/schema';
+
+import { FlowEdge, FlowElements } from '../interface/element';
+
+import { createEdgeMarker } from './element-helper';
+
+export function prepareSpecialRepeatEventElements(
+  eventName: string,
+  event: FlowEventRepeat,
+): FlowElements {
+  let edge: FlowEdge | null = null;
+
+  if (event.startEventName) {
+    edge = {
+      id: `${eventName}>${event.startEventName}`,
+      source: eventName,
+      target: event.startEventName,
+      markerEnd: createEdgeMarker(),
+    };
+  }
+
+  return {
+    nodes: [],
+    edges: edge ? [edge] : [],
+  };
+}
--- apps/editor/src/pages/flow/use-generate-elements.ts
+++ apps/editor/src/pages/flow/use-generate-elements.ts
@@ -1,22 +1,17 @@
 import { FlowEvent } from '@shukun/schema';
 import { useObservableState } from 'observable-hooks';
-import { EdgeMarker, MarkerType } from 'reactflow';
 
 import { flowQuery } from '../../services/flow';
 
-import { eventSchemas } from './flow-event-schemas';
-import { eventUI } from './flow-event-ui';
+import { prepareSpecialChoiceEventElements } from './elements/choice-elements';
 
-import {
-  FlowCustomNodeName,
-  FlowEdge,
-  FlowElements,
-  FlowNode,
-} from './interface/element';
+import { prepareCommonEventElements } from './elements/common-elements';
 
-export const START_NODE_NAME = '$$start$$';
-export const END_NODE_NAME = '$$end$$';
-export const WRONG_NODE_NAME = '$$wrong$$';
+import { prepareFunctionalElements } from './elements/functional-elements';
+import { prepareSpecialParallelEventElements } from './elements/parallel-elements';
+import { prepareSpecialRepeatEventElements } from './elements/repeat-elements';
+
+import { FlowElements } from './interface/element';
 
 export function useGenerateElement(): FlowElements {
   const flow = useObservableState(flowQuery.activeFlow$);
@@ -33,10 +28,16 @@ export function useGenerateElement(): FlowElements {
   const { startEventName, events } = flow;
 
   for (const [eventName, event] of Object.entries(events)) {
-    const { nodes, edges } = prepareEventElements(eventName, event);
+    const { nodes, edges } = prepareCommonEventElements(eventName, event);
 
     elements.nodes = [...elements.nodes, ...nodes];
     elements.edges = [...elements.edges, ...edges];
+
+    {
+      const { nodes, edges } = prepareSpecialEventElements(eventName, event);
+      elements.nodes = [...elements.nodes, ...nodes];
+      elements.edges = [...elements.edges, ...edges];
+    }
   }
 
   {
@@ -46,151 +47,24 @@ export function useGenerateElement(): FlowElements {
     elements.edges = [...elements.edges, ...edges];
   }
 
-  // prepareEndElement()
-  // prepareWrongElement()
-
   return elements;
 }
 
-function prepareEventElements(
+function prepareSpecialEventElements(
   eventName: string,
   event: FlowEvent,
 ): FlowElements {
   switch (event.type) {
-    case 'Repeat':
     case 'Choice':
+      return prepareSpecialChoiceEventElements(eventName, event);
     case 'Parallel':
-      return { nodes: [], edges: [] };
+      return prepareSpecialParallelEventElements(eventName, event);
+    case 'Repeat':
+      return prepareSpecialRepeatEventElements(eventName, event);
     default:
-      return prepareCommonEventElements(eventName, event);
-  }
-}
-
-function prepareCommonEventElements(
-  eventName: string,
-  event: FlowEvent,
-): FlowElements {
-  const node: FlowNode = {
-    id: eventName,
-    position: getDefaultPosition(),
-    data: {
-      label: eventName,
-      eventName,
-      event,
-      eventSchema: eventSchemas[event.type],
-      eventUI: eventUI[event.type],
-    },
-    type: FlowCustomNodeName.EventNode,
-  };
-
-  let edge: FlowEdge | null = null;
-
-  if (typeof event.next === 'string' && event.next) {
-    const flow = flowQuery.getCloneActiveFlow();
-    const isExist = flowQuery.existEvent(flow, event.next);
-
-    if (isExist) {
-      edge = {
-        id: `${eventName}>${event.next}`,
-        source: eventName,
-        target: event.next,
-        markerEnd: createEdgeMarker(),
+      return {
+        nodes: [],
+        edges: [],
       };
-    } else {
-      edge = {
-        id: `${eventName}>${WRONG_NODE_NAME}`,
-        source: eventName,
-        target: WRONG_NODE_NAME,
-        markerEnd: createEdgeMarker(),
-      };
-    }
-  } else if (!event.next) {
-    edge = {
-      id: `${eventName}>${END_NODE_NAME}`,
-      source: eventName,
-      target: END_NODE_NAME,
-      markerEnd: createEdgeMarker(),
-    };
   }
-
-  return {
-    nodes: [node],
-    edges: edge ? [edge] : [],
-  };
-}
-
-function prepareFunctionalElements(startName: string): FlowElements {
-  const startNode: FlowNode = {
-    id: START_NODE_NAME,
-    position: getDefaultPosition(),
-    data: {
-      label: 'Start',
-      eventUI: {
-        width: 30,
-        height: 30,
-        backgroundColor: '#000',
-        fontColor: '#fff',
-      },
-    },
-    type: FlowCustomNodeName.FunctionalNode,
-  };
-
-  const endNode: FlowNode = {
-    id: END_NODE_NAME,
-    position: getDefaultPosition(),
-    data: {
-      label: 'End',
-      eventUI: {
-        width: 30,
-        height: 30,
-        backgroundColor: 'green',
-        fontColor: '#fff',
-      },
-    },
-    type: FlowCustomNodeName.FunctionalNode,
-  };
-
-  const wrongNode: FlowNode = {
-    id: WRONG_NODE_NAME,
-    position: getDefaultPosition(),
-    data: {
-      label: 'End',
-      eventUI: {
-        width: 30,
-        height: 30,
-        backgroundColor: 'red',
-        fontColor: '#fff',
-      },
-    },
-    type: FlowCustomNodeName.FunctionalNode,
-  };
-
-  let startEdge: FlowEdge | null = null;
-
-  if (startName) {
-    startEdge = {
-      id: `${START_NODE_NAME}>${startName}`,
-      source: START_NODE_NAME,
-      target: startName,
-      markerEnd: createEdgeMarker(),
-    };
-  }
-
-  return {
-    nodes: [startNode, endNode, wrongNode],
-    edges: startEdge ? [startEdge] : [],
-  };
-}
-
-function getDefaultPosition() {
-  return { x: 0, y: 0 };
-}
-
-function createEdgeMarker() {
-  const edgeMarker: EdgeMarker = {
-    type: MarkerType.ArrowClosed,
-    width: 18,
-    height: 18,
-  };
-  return edgeMarker;
 }
