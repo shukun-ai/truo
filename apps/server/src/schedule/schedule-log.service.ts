9683cd976f0c987b069ef4e7097dea4d3e16fcf9
Chase Wu <minowu@foxmail.com>
2022-12-30 19:37:32 +0800

    feat: add schedule-log now.

--- apps/server/src/schedule/schedule-log.service.ts
+++ apps/server/src/schedule/schedule-log.service.ts
@@ -4,20 +4,29 @@ import { ScheduleSchema } from '@shukun/schema';
 @Injectable()
 export class ScheduleLogService {
   logSuccess(orgName: string, schedule: ScheduleSchema, result: unknown) {
+    const now = new Date();
     // eslint-disable-next-line no-console
-    console.log('The job was run successfully', result, { orgName, schedule });
+    console.log('The job was run successfully', result, {
+      orgName,
+      schedule,
+      now,
+    });
   }
 
   logException(orgName: string, schedule: ScheduleSchema, exception: unknown) {
+    const now = new Date();
+
     console.error({
       orgName,
       schedule,
       exception,
+      now,
     });
   }
 
   logComplete(orgName: string, schedule: ScheduleSchema) {
+    const now = new Date();
     // eslint-disable-next-line no-console
-    console.log('The job was completed', { orgName, schedule });
+    console.log('The job was completed', { orgName, schedule, now });
   }
 }
