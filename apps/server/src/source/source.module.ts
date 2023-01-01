904a23017d9abe988267b7b2584f8ad9c681de9e
Chase Wu <minowu@foxmail.com>
2023-01-01 10:16:23 +0800

    refactor: move mongo service.

--- apps/server/src/source/source.module.ts
+++ apps/server/src/source/source.module.ts
@@ -3,9 +3,9 @@ import { Module } from '@nestjs/common';
 import { CoreModule } from '../core/core.module';
 import { PassportModule } from '../util/passport/passport.module';
 
-import { MongoAdaptorService } from './adaptor/mongo-adaptor.service';
-import { MongoQueryConvertorService } from './adaptor/mongo-query-convertor.service';
-import { MongooseConnectionService } from './adaptor/mongoose-connection.service';
+import { MongoAdaptorService } from './mongo/mongo-adaptor.service';
+import { MongoQueryConvertorService } from './mongo/mongo-query-convertor.service';
+import { MongooseConnectionService } from './mongo/mongoose-connection.service';
 import { SourceDataAccessService } from './source-data-access.service';
 import { SourceForeignQueryService } from './source-foreign-query.service';
 import { SourceFoundationService } from './source-foundation.service';
