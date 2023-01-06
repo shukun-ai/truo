f30afd65e8bbcf2783f5bf1c737084f24efbeec0
Chase Wu <minowu@foxmail.com>
2023-01-06 11:19:29 +0800

    feat: update errorMessages.

--- apps/server/src/source/electron/fields/name-text.field.ts
+++ apps/server/src/source/electron/fields/name-text.field.ts
@@ -1,18 +1,19 @@
 import { isEngineName, isStartedWithLowercase } from '@shukun/electron';
+import { MetadataElectron } from '@shukun/schema';
 import { Schema } from 'mongoose';
 
 import { ElectronType, SchemaBuilderResult } from '../electron-field.interface';
 
 export class NameTextField implements ElectronType {
-  validateValue(value: unknown): string[] {
+  validateValue(value: unknown, electron: MetadataElectron): string[] {
     const errorMessage = [];
 
     if (!isEngineName(value)) {
-      errorMessage.push(`should be a-z, 0-9 or _ .`);
+      errorMessage.push(`${electron.name} 字段仅允许 a-z, 0-9 或 _ 字符。`);
     }
 
     if (!isStartedWithLowercase(value)) {
-      errorMessage.push('should be started with a-z.');
+      errorMessage.push(`${electron.name} 字段仅允许以 a-z 作为第一个字符。`);
     }
 
     return errorMessage;
--- apps/server/src/source/electron/fields/password.field.ts
+++ apps/server/src/source/electron/fields/password.field.ts
@@ -1,3 +1,7 @@
+import {
+  PASSWORD_DEFAULT_MAX_LENGTH,
+  PASSWORD_DEFAULT_MIN_LENGTH,
+} from '@shukun/electron';
 import { MetadataElectron } from '@shukun/schema';
 import { Schema } from 'mongoose';
 
@@ -7,16 +11,13 @@ import { cryptoPassword } from '../../../identity/utils/password.utils';
 
 import { ElectronType, SchemaBuilderResult } from '../electron-field.interface';
 
-const DEFAULT_PASSWORD_MIN_LENGTH = 6;
-const DEFAULT_PASSWORD_MAX_LENGTH = 99;
-
 export class PasswordField implements ElectronType {
   validateValue(value: unknown, electron: MetadataElectron) {
     const messages = [];
     const minLength =
-      electron.passwordOptions?.minLength || DEFAULT_PASSWORD_MIN_LENGTH;
+      electron.passwordOptions?.minLength || PASSWORD_DEFAULT_MIN_LENGTH;
     const maxLength =
-      electron.passwordOptions?.maxLength || DEFAULT_PASSWORD_MAX_LENGTH;
+      electron.passwordOptions?.maxLength || PASSWORD_DEFAULT_MAX_LENGTH;
 
     if (!value || typeof value !== 'string') {
       messages.push('密码输入值应为非空字符串。');
