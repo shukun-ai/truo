e84a6f9bb98eb458101bd1e815edf8dc40dbdc9e
Chase Wu <minowu@foxmail.com>
2023-01-06 12:36:53 +0800

    feat: add PostgresExceptionHandlerService.

---
+++ apps/server/src/source/postgres/postgres-exception-handler.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+
+@Injectable()
+export class PostgresExceptionHandlerService {
+  handle(error: unknown): Error {
+    if (
+      typeof error === 'object' &&
+      error !== null &&
+      'severity' in error &&
+      error.severity === 'ERROR' &&
+      'code' in error &&
+      error.code === '23505'
+    ) {
+      return this.handlePostgresDuplicationException(error);
+    }
+
+    return new InternalServerErrorException(
+      '发生未知错误，错误信息是' + error?.toString(),
+    );
+  }
+
+  /* @example
+  {
+    length: 213,
+    severity: 'ERROR',
+    code: '23505',
+    detail: 'Key (number)=(Su-001) already exists.',
+    hint: undefined,
+    position: undefined,
+    internalPosition: undefined,
+    internalQuery: undefined,
+    where: undefined,
+    schema: 'public',
+    table: 'devices',
+    column: undefined,
+    dataType: undefined,
+    constraint: 'devices_number_unique',
+    file: 'nbtinsert.c',
+    line: '671',
+    routine: '_bt_check_unique'
+  }
+  */
+  handlePostgresDuplicationException(error: unknown): Error {
+    const message =
+      typeof error === 'object' && error !== null && 'detail' in error
+        ? error.detail
+        : '';
+    return new Error(`存在唯一字段重复值：${message}`);
+  }
+}
