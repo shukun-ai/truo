f80e610fe854fdb2646aa5698c2ce13eb1e705e4
Chase Wu <minowu@foxmail.com>
2023-01-07 09:34:14 +0800

    feat: add applicationAjv and baseAjv.

--- libs/schema/src/validators/workflow-configurations/validate.ts
+++ libs/schema/src/validators/workflow-configurations/validate.ts
@@ -1,8 +1,10 @@
 import { WorkflowConfigurations } from '../../types/application';
-import { createAjv } from '../constructor/validate';
+import { createApplicationAjv } from '../constructor/application-ajv';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const workflowConfigurationsSchema = require('../../json-schemas/workflows/configurations.schema.json');
 
 export const validateWorkflowConfigurations =
-  createAjv().compile<WorkflowConfigurations>(workflowConfigurationsSchema);
+  createApplicationAjv().compile<WorkflowConfigurations>(
+    workflowConfigurationsSchema,
+  );
