3339a48aef15e67ef7c58f45442101a982de5eda
Chase Wu <minowu@foxmail.com>
2023-01-07 12:17:38 +0800

    feat: add dataSourceConnection.

--- apps/server/src/source/knex/knex-connection.service.ts
+++ apps/server/src/source/knex/knex-connection.service.ts
@@ -1,27 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { MetadataSchema } from '@shukun/schema';
+import { DataSourceConnection, MetadataSchema } from '@shukun/schema';
 import knex from 'knex';
 
-import { OrgService } from '../../core/org.service';
 import { TypeException } from '../../exceptions/type-exception';
 
 @Injectable()
 export class KnexConnectionService {
-  constructor(private readonly orgService: OrgService) {}
-
-  async getClient(orgName: string, metadata: MetadataSchema) {
-    if (!metadata.source) {
-      throw new TypeException(
-        'The metadata source is not defined: {{source}}',
-        { source: metadata.source },
-      );
+  async getClient(dataSourceConnection: DataSourceConnection | null) {
+    if (!dataSourceConnection) {
+      throw new TypeException('The metadata data source is not defined');
     }
 
     const client = knex({
       client: 'pg',
-      connection: metadata.source,
+      connection: dataSourceConnection.connection,
       pool: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         afterCreate: (connection: any, callback: any) => {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
           connection.query('SET timezone = "UTC";', function (error: any) {
             callback(error, connection);
           });
@@ -32,7 +28,15 @@ export class KnexConnectionService {
     return client;
   }
 
-  getTableName(orgName: string, metadata: MetadataSchema) {
-    return metadata.name;
+  getTableName(
+    orgName: string,
+    metadata: MetadataSchema,
+    dataSourceConnection: DataSourceConnection | null,
+  ) {
+    if (!dataSourceConnection?.tablePrefix) {
+      return metadata.name;
+    }
+
+    return `${dataSourceConnection.tablePrefix}${metadata.name}`;
   }
 }
