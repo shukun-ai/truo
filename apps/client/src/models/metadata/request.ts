05f8c04b3af851f80b8686065e7e11475e8e0344
Chase Wu <minowu@foxmail.com>
2023-01-09 18:40:51 +0800

    feat: remove select: '_all' in metadata request.

--- apps/client/src/models/metadata/request.ts
+++ apps/client/src/models/metadata/request.ts
@@ -3,13 +3,13 @@ import { MetadataSchema, RoleResourceType } from '@shukun/schema';
 
 import { httpRequestService } from '../../utils/http-helper';
 
-// @todo should use core api not source api for getting metadata.
+// TODO should use core api not source api for getting metadata.
 export async function findOneMetadata(source: string, params?: QueryParams) {
   const response = await httpRequestService
     .createAxios()
     .get<ApiResponseData<MetadataSchema>>(
       `${RoleResourceType.Source}/:orgName/${source}/metadata`,
-      { params: { ...params, select: '_all' } },
+      { params },
     );
   return response;
 }
