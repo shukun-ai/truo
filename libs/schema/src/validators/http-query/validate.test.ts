7edd917c83ee7cadef23da14499b5f0175caacbf
Chase Wu <minowu@foxmail.com>
2023-01-10 17:47:04 +0800

    feat: add query filter validate.

--- libs/schema/src/validators/http-query/validate.test.ts
+++ libs/schema/src/validators/http-query/validate.test.ts
@@ -1,10 +1,10 @@
 import { inspectTestingValidate } from '../../testing/testing-validate-inspector';
-import { HttpQuerySchema } from '../../types/http-query';
+import { HttpQuerySchema } from '../../types/http-query-override';
 
-import { validateHttpQuerySchema } from './validate';
+import { validateHttpQuerySchema, validateQueryFilter } from './validate';
 
 describe('validateHttpQuerySchema', () => {
-  it('should pass when simple filter', () => {
+  it('should pass when simple filter.', () => {
     const query: HttpQuerySchema = {
       filter: {
         name: {
@@ -15,9 +15,11 @@ describe('validateHttpQuerySchema', () => {
     };
     const result = validateHttpQuerySchema(query);
     expect(result).toEqual(true);
+
+    expect(validateQueryFilter(query)).toEqual(undefined);
   });
 
-  it('should pass when simple filter', () => {
+  it('should pass when $or under electron name.', () => {
     const query: HttpQuerySchema = {
       filter: {
         name: {
@@ -35,9 +37,11 @@ describe('validateHttpQuerySchema', () => {
     };
     const result = validateHttpQuerySchema(query);
     expect(result).toEqual(true);
+
+    expect(validateQueryFilter(query)).toEqual(undefined);
   });
 
-  it('should pass when top or filter', () => {
+  it('should pass when $or in top level.', () => {
     const query: HttpQuerySchema = {
       filter: {
         $or: [
@@ -54,9 +58,11 @@ describe('validateHttpQuerySchema', () => {
     const result = validateHttpQuerySchema(query);
     inspectTestingValidate(result, validateHttpQuerySchema);
     expect(result).toEqual(true);
+
+    expect(validateQueryFilter(query)).toEqual(undefined);
   });
 
-  it('should pass when top or filter', () => {
+  it('should pass when $or in top level and name and vehicle combined structure.', () => {
     const query: HttpQuerySchema = {
       filter: {
         $or: [
@@ -74,9 +80,11 @@ describe('validateHttpQuerySchema', () => {
     const result = validateHttpQuerySchema(query);
     inspectTestingValidate(result, validateHttpQuerySchema);
     expect(result).toEqual(true);
+
+    expect(validateQueryFilter(query)).toEqual(undefined);
   });
 
-  it('should pass when top or filter', () => {
+  it('should pass when complex or.', () => {
     const query: HttpQuerySchema = {
       filter: {
         $or: [
@@ -98,5 +106,52 @@ describe('validateHttpQuerySchema', () => {
     const result = validateHttpQuerySchema(query);
     inspectTestingValidate(result, validateHttpQuerySchema);
     expect(result).toEqual(true);
+
+    expect(validateQueryFilter(query)).toEqual(undefined);
+  });
+
+  it('should throw error, when use $set not allowed character.', () => {
+    const query: HttpQuerySchema = {
+      filter: {
+        $set: [
+          {
+            name: 'system',
+            vehicle: {
+              $eq: 's',
+              $in: ['hi'],
+            },
+          },
+          {
+            age: 30,
+          },
+        ],
+      },
+      limit: 10,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any;
+
+    expect(() => validateQueryFilter(query)).toThrow(
+      new Error(`The key is not allowed: $set.`),
+    );
+  });
+
+  it('should throw error, when the value of $and is not a array.', () => {
+    const query: HttpQuerySchema = {
+      filter: {
+        $and: {
+          name: 'system',
+          vehicle: {
+            $eq: 's',
+            $in: ['hi'],
+          },
+        },
+      },
+      limit: 10,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any;
+
+    expect(() => validateQueryFilter(query)).toThrow(
+      new Error(`The value should be a array after '$and' or '$or'.`),
+    );
   });
 });
