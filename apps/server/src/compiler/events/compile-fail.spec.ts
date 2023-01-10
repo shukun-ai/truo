c56745333f51a908d8367590b65cd57d4bfa2ae5
Chase Wu <minowu@foxmail.com>
2023-01-10 10:45:51 +0800

    feat: add unit testings for compile events.

---
+++ apps/server/src/compiler/events/compile-fail.spec.ts
@@ -0,0 +1,37 @@
+import { FlowEventFail } from '@shukun/schema';
+
+import { SandboxService } from '../../sandbox/sandbox.service';
+
+import {
+  createTestingSandbox,
+  createTestingContext,
+} from '../../util/unit-testing/sandbox-testing.helper';
+
+import { compileFailEvent } from './compile-fail';
+
+describe('compileFailEvent', () => {
+  let sandboxService: SandboxService;
+  let event: FlowEventFail;
+
+  beforeAll(() => {
+    const testingSandbox = createTestingSandbox();
+    sandboxService = testingSandbox.sandboxService;
+
+    event = {
+      type: 'Fail',
+      output: `{ name: 'mockName' }`,
+    };
+  });
+
+  it('Should return new next and input.', async () => {
+    const context = createTestingContext();
+    const code = await compileFailEvent(event);
+    const computedContext = await sandboxService.executeVM(code, context);
+
+    expect(computedContext).toEqual({
+      ...context,
+      input: { name: 'mockName' },
+      next: '',
+    });
+  });
+});
