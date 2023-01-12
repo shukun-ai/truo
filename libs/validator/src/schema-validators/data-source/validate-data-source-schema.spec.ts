f5a4980f0e0c81ad7269cb134986ec5d91a7cd5b
Chase Wu <minowu@foxmail.com>
2023-01-12 12:49:47 +0800

    feat: rename validates.

--- libs/validator/src/schema-validators/data-source/validate-data-source-schema.spec.ts
+++ libs/validator/src/schema-validators/data-source/validate-data-source-schema.spec.ts
@@ -1,7 +1,7 @@
 import { inspectTestingValidate } from '../../testing-helpers/testing-validate-inspector';
 
 import dataSourceTest from './data-source.test.json';
-import { validateDataSourceSchema } from './validate';
+import { validateDataSourceSchema } from './validate-data-source-schema';
 
 describe('attachment', () => {
   it('validateDataSourceSchema', () => {
