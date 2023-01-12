e000d56e8458e0aff0c78cf8a3b70b82221ef5e4
Chase Wu <minowu@foxmail.com>
2023-01-12 14:58:07 +0800

    feat: use latest validator.

--- libs/cli/src/generate/helpers/validate.ts
+++ libs/cli/src/generate/helpers/validate.ts
@@ -1,15 +1,9 @@
 import { ApplicationSchema } from '@shukun/schema';
-import { validateApplicationSchema } from '@shukun/validator';
+import { applicationSchemaValidator } from '@shukun/validator';
 
 export async function validate(
   application: ApplicationSchema,
 ): Promise<ApplicationSchema> {
-  const result = validateApplicationSchema(application);
-
-  if (!result) {
-    console.error(validateApplicationSchema.errors);
-    throw new Error('Failed to validate this application schema.');
-  }
-
+  applicationSchemaValidator.validate(application);
   return application;
 }
