1678f39b1566d17b0dce9f34663fa1cf4b0d72af
Chase Wu <minowu@foxmail.com>
2023-01-14 10:55:22 +0800

    test: add authorization unit testing.

--- apps/server/src/api/authorization/authorization.service.spec.ts
+++ apps/server/src/api/authorization/authorization.service.spec.ts
@@ -195,11 +195,103 @@ describe('AuthorizationService', () => {
     });
   });
 
-  // GET    /apis/v1/view/{orgName}/views/{viewName}
-  // POST   /apis/v1/developer/{orgName}/codebase
-  // POST   /apis/v1/developer/{orgName}/data-source
-  // POST   /apis/v1/source/{orgName}/{atomName}/any/metadata
-  // GET    /apis/v1/source/{orgName}/{atomName}/metadata
+  describe('POST /apis/v1/developer/{orgName}/codebase', () => {
+    it('if owner, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/developer/shukun/codebase',
+        validOwnerUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if coo, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/developer/shukun/codebase',
+        validCooUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if cfo, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/developer/shukun/codebase',
+          validCfoUserToken,
+        ),
+      ).rejects.toThrow(new ForbiddenException('未授权访问该资源。'));
+    });
+
+    it('if anonymous metadata orders, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/developer/shukun/codebase',
+          anonymousUserToken,
+        ),
+      ).rejects.toThrow(new UnauthorizedException('未登录无法访问该资源。'));
+    });
+  });
+
+  describe('POST /apis/v1/source/{orgName}/{atomName}/any/metadata', () => {
+    it('if owner, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/metadata',
+        validOwnerUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if coo, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/metadata',
+        validCooUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if cfo, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/metadata',
+          validCfoUserToken,
+        ),
+      ).rejects.toThrow(new ForbiddenException('未授权访问该资源。'));
+    });
+
+    it('if cfo has readOwn of payments, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/payments/any/metadata',
+        validCfoUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if anonymous metadata orders, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/metadata',
+          anonymousUserToken,
+        ),
+      ).rejects.toThrow(new UnauthorizedException('未登录无法访问该资源。'));
+    });
+
+    it('if anonymous has read of products, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/products/any/metadata',
+        anonymousUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+  });
 
   describe('POST /apis/v1/source/{orgName}/{atomName}/any/query', () => {
     it('if owner, then pass.', async () => {
@@ -220,7 +312,7 @@ describe('AuthorizationService', () => {
       expect(output).toBeUndefined();
     });
 
-    it('if cfo, then throw2.', async () => {
+    it('if cfo, then throw.', async () => {
       await expect(
         authorizationService.validate(
           'POST',
@@ -239,6 +331,16 @@ describe('AuthorizationService', () => {
       expect(output).toBeUndefined();
     });
 
+    it('if anonymous query orders, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/query',
+          anonymousUserToken,
+        ),
+      ).rejects.toThrow(new UnauthorizedException('未登录无法访问该资源。'));
+    });
+
     it('if anonymous has read of products, then pass.', async () => {
       const output = await authorizationService.validate(
         'POST',
@@ -250,14 +352,377 @@ describe('AuthorizationService', () => {
   });
 
   // POST   /apis/v1/source/{orgName}/{atomName}/any/create
-  // POST   /apis/v1/source/{orgName}/{atomName}/{id}/update
-  // POST   /apis/v1/source/{orgName}/{atomName}/{id}/add-to-many
-  // POST   /apis/v1/source/{orgName}/{atomName}/{id}/remove-from-many
-  // POST   /apis/v1/source/{orgName}/{atomName}/{id}/increase
-  // POST   /apis/v1/source/{orgName}/{atomName}/{id}/delete
-  // POST   /apis/v1/tenant/any/seeds
-  // GET    /apis/v1/public/{orgName}/roles
-  // GET    /apis/v1/developer/{orgName}/inspection
+  describe('POST /apis/v1/source/{orgName}/{atomName}/any/create', () => {
+    it('if owner, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/create',
+        validOwnerUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if coo, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/create',
+        validCooUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if cfo, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/create',
+          validCfoUserToken,
+        ),
+      ).rejects.toThrow(new ForbiddenException('未授权访问该资源。'));
+    });
+
+    it('if anonymous query orders, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/create',
+          anonymousUserToken,
+        ),
+      ).rejects.toThrow(new UnauthorizedException('未登录无法访问该资源。'));
+    });
+  });
+
+  describe('POST /apis/v1/source/{orgName}/{atomName}/any/update', () => {
+    it('if owner, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/update',
+        validOwnerUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if coo, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/update',
+        validCooUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if cfo, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/update',
+          validCfoUserToken,
+        ),
+      ).rejects.toThrow(new ForbiddenException('未授权访问该资源。'));
+    });
+
+    it('if cfo has readOwn of payments, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/payments/any/update',
+        validCfoUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if anonymous update orders, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/update',
+          anonymousUserToken,
+        ),
+      ).rejects.toThrow(new UnauthorizedException('未登录无法访问该资源。'));
+    });
+
+    it('if anonymous has read of products, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/products/any/update',
+        anonymousUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+  });
+
+  describe('POST /apis/v1/source/{orgName}/{atomName}/any/add-to-many', () => {
+    it('if owner, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/add-to-many',
+        validOwnerUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if coo, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/add-to-many',
+        validCooUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if cfo, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/add-to-many',
+          validCfoUserToken,
+        ),
+      ).rejects.toThrow(new ForbiddenException('未授权访问该资源。'));
+    });
+
+    it('if cfo has readOwn of payments, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/payments/any/add-to-many',
+        validCfoUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if anonymous add-to-many orders, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/add-to-many',
+          anonymousUserToken,
+        ),
+      ).rejects.toThrow(new UnauthorizedException('未登录无法访问该资源。'));
+    });
+
+    it('if anonymous has read of products, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/products/any/add-to-many',
+        anonymousUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+  });
+
+  describe('POST /apis/v1/source/{orgName}/{atomName}/any/remove-from-many', () => {
+    it('if owner, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/remove-from-many',
+        validOwnerUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if coo, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/remove-from-many',
+        validCooUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if cfo, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/remove-from-many',
+          validCfoUserToken,
+        ),
+      ).rejects.toThrow(new ForbiddenException('未授权访问该资源。'));
+    });
+
+    it('if cfo has readOwn of payments, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/payments/any/remove-from-many',
+        validCfoUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if anonymous remove-from-many orders, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/remove-from-many',
+          anonymousUserToken,
+        ),
+      ).rejects.toThrow(new UnauthorizedException('未登录无法访问该资源。'));
+    });
+
+    it('if anonymous has read of products, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/products/any/remove-from-many',
+        anonymousUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+  });
+
+  describe('POST /apis/v1/source/{orgName}/{atomName}/any/increase', () => {
+    it('if owner, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/increase',
+        validOwnerUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if coo, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/increase',
+        validCooUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if cfo, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/increase',
+          validCfoUserToken,
+        ),
+      ).rejects.toThrow(new ForbiddenException('未授权访问该资源。'));
+    });
+
+    it('if cfo has readOwn of payments, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/payments/any/increase',
+        validCfoUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if anonymous increase orders, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/increase',
+          anonymousUserToken,
+        ),
+      ).rejects.toThrow(new UnauthorizedException('未登录无法访问该资源。'));
+    });
+
+    it('if anonymous has read of products, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/products/any/increase',
+        anonymousUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+  });
+
+  describe('POST /apis/v1/source/{orgName}/{atomName}/any/delete', () => {
+    it('if owner, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/delete',
+        validOwnerUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if coo, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/orders/any/delete',
+        validCooUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if cfo, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/delete',
+          validCfoUserToken,
+        ),
+      ).rejects.toThrow(new ForbiddenException('未授权访问该资源。'));
+    });
+
+    it('if cfo has readOwn of payments, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/payments/any/delete',
+        validCfoUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+
+    it('if anonymous delete orders, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/source/shukun/orders/any/delete',
+          anonymousUserToken,
+        ),
+      ).rejects.toThrow(new UnauthorizedException('未登录无法访问该资源。'));
+    });
+
+    it('if anonymous has read of products, then pass.', async () => {
+      const output = await authorizationService.validate(
+        'POST',
+        '/apis/v1/source/shukun/products/any/delete',
+        anonymousUserToken,
+      );
+      expect(output).toBeUndefined();
+    });
+  });
+
+  describe('POST /apis/v1/tenant/any/seeds', () => {
+    it('if owner, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/tenant/any/seeds',
+          validOwnerUserToken,
+        ),
+      ).rejects.toThrow(new ForbiddenException('没有权限操作内部接口。'));
+    });
+
+    it('if coo, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/tenant/any/seeds',
+          validCooUserToken,
+        ),
+      ).rejects.toThrow(new ForbiddenException('没有权限操作内部接口。'));
+    });
+
+    it('if cfo, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/tenant/any/seeds',
+          validCfoUserToken,
+        ),
+      ).rejects.toThrow(new ForbiddenException('没有权限操作内部接口。'));
+    });
+
+    it('if anonymous add-to-many orders, then throw.', async () => {
+      await expect(
+        authorizationService.validate(
+          'POST',
+          '/apis/v1/tenant/any/seeds',
+          anonymousUserToken,
+        ),
+      ).rejects.toThrow(new ForbiddenException('没有权限操作内部接口。'));
+    });
+  });
 
   describe('if internal, always throw error.', () => {
     it('if resource type is view, then always pass.', async () => {
