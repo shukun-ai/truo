4f1270b2e01fc96e524dbd96aeeaf791ca00a820
Chase Wu <minowu@foxmail.com>
2023-01-14 09:34:51 +0800

    feat: remove legacy source api.

--- apps/server/src/api/source/source.controller.ts
+++ apps/server/src/api/source/source.controller.ts
@@ -1,13 +1,10 @@
 import {
   Body,
   Controller,
-  Get,
   Post,
   UseInterceptors,
   Inject,
   Param,
-  Put,
-  Delete,
   Req,
 } from '@nestjs/common';
 import { HttpQuerySchema, IDString, RoleResourceType } from '@shukun/schema';
@@ -15,9 +12,8 @@ import { HttpQuerySchema, IDString, RoleResourceType } from '@shukun/schema';
 import { SourceServiceCreateDto } from '../../app.type';
 import { SecurityRequest } from '../../identity/utils/security-request';
 import { ParsedHttpQuery } from '../../util/query/decorators/parsed-http-query.decorator';
-import { ParsedQuery } from '../../util/query/decorators/parsed-query.decorator';
 import { QueryResponseInterceptor } from '../../util/query/interceptors/query-response.interceptor';
-import { QueryParserOptions, QueryResponse } from '../../util/query/interfaces';
+import { QueryResponse } from '../../util/query/interfaces';
 
 import { AddToManyDto } from './dto/add-to-many.dto';
 import { IncreaseDto } from './dto/increase.dto';
@@ -37,36 +33,11 @@ export class SourceController {
     return await this.sourceOperationService.getMetadata(orgName, atomName);
   }
 
-  // @deprecated
-  @Get('metadata')
-  async metadata(
-    @Param('orgName') orgName: string,
-    @Param('atomName') atomName: string,
-  ) {
-    return await this.sourceOperationService.getMetadata(orgName, atomName);
-  }
-
   @Post('any/query')
   async query(
     @Param('orgName') orgName: string,
     @Param('atomName') atomName: string,
     @ParsedHttpQuery() query: HttpQuerySchema,
-    @Req() request: SecurityRequest,
-  ): Promise<QueryResponse<unknown>> {
-    return await this.sourceOperationService.query(
-      orgName,
-      atomName,
-      query,
-      request,
-    );
-  }
-
-  // @deprecated
-  @Get()
-  async index(
-    @Param('orgName') orgName: string,
-    @Param('atomName') atomName: string,
-    @ParsedQuery() query: QueryParserOptions,
     @Req() request: SecurityRequest,
   ): Promise<QueryResponse<unknown>> {
     return await this.sourceOperationService.query(
@@ -92,42 +63,8 @@ export class SourceController {
     );
   }
 
-  // @deprecated
-  @Post()
-  async create(
-    @Param('orgName') orgName: string,
-    @Param('atomName') atomName: string,
-    @Body() createDto: SourceServiceCreateDto,
-    @Req() request: SecurityRequest,
-  ): Promise<QueryResponse<{ _id: IDString }>> {
-    return await this.sourceOperationService.create(
-      orgName,
-      atomName,
-      createDto,
-      request.userId ?? null,
-    );
-  }
-
   @Post(':id/update')
   async updateOne(
-    @Param('id') id: string,
-    @Param('orgName') orgName: string,
-    @Param('atomName') atomName: string,
-    @Body() createDto: SourceServiceCreateDto,
-    @Req() request: SecurityRequest,
-  ): Promise<QueryResponse<null>> {
-    return await this.sourceOperationService.update(
-      id,
-      orgName,
-      atomName,
-      createDto,
-      request.userId ?? null,
-    );
-  }
-
-  // @deprecated
-  @Put(':id')
-  async update(
     @Param('id') id: string,
     @Param('orgName') orgName: string,
     @Param('atomName') atomName: string,
@@ -190,16 +127,6 @@ export class SourceController {
 
   @Post(':id/delete')
   async deleteOne(
-    @Param('id') id: string,
-    @Param('orgName') orgName: string,
-    @Param('atomName') atomName: string,
-  ): Promise<QueryResponse<null>> {
-    return await this.sourceOperationService.delete(id, orgName, atomName);
-  }
-
-  // @deprecated
-  @Delete(':id')
-  async delete(
     @Param('id') id: string,
     @Param('orgName') orgName: string,
     @Param('atomName') atomName: string,
