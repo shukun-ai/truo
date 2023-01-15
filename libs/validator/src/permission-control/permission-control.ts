a8b26ff925188ec699d1fceaa49354893af682b2
Chase Wu <minowu@foxmail.com>
2023-01-15 11:45:35 +0800

    feat: extract from permission-control.

--- libs/validator/src/permission-control/permission-control.ts
+++ libs/validator/src/permission-control/permission-control.ts
@@ -1,84 +1,28 @@
 import { RoleSchema } from '@shukun/schema';
 
-import { union } from 'lodash';
-
 import { IPermissionControl } from './permission-control.interface';
-import {
-  GrantedRoles,
-  PermissionNodes,
-  SOURCE_QUERY_FAMILY,
-  SOURCE_UPDATE_FAMILY,
-} from './permission-control.type';
+import { GrantedRoles, PermissionNodes } from './permission-control.type';
 
 import { PermissionConvertor } from './permission-convertor';
+import { PermissionGranter } from './permission-granter';
+import { PermissionParser } from './permission-parser';
 
 export class PermissionControl implements IPermissionControl {
   private permissions: PermissionNodes[];
+
+  private granter: PermissionGranter;
 
   constructor(
     private readonly roles: RoleSchema[],
     private readonly grantedRoles: GrantedRoles,
   ) {
-    this.permissions = this.createPermissions();
+    const permissionConvertor = new PermissionConvertor();
+    const permissionParser = new PermissionParser(permissionConvertor);
+    this.permissions = permissionParser.parse(this.roles, this.grantedRoles);
+    this.granter = new PermissionGranter(this.permissions);
   }
 
   public grant(type: string, name: string, action?: string): boolean {
-    return this.permissions.some(
-      (permission) =>
-        permission.type === type &&
-        permission.name === name &&
-        permission.action === this.validateAction(permission, action),
-    );
-  }
-
-  private validateAction(
-    permission: PermissionNodes,
-    action?: string,
-  ): string | null {
-    if (this.isSourceQuery(permission, action)) {
-      return 'query';
-    }
-
-    if (this.isSourceUpdate(permission, action)) {
-      return 'update';
-    }
-
-    return action ?? null;
-  }
-
-  private isSourceQuery(permission: PermissionNodes, action?: string): boolean {
-    return (
-      !!action &&
-      permission.type === 'source' &&
-      permission.action === 'query' &&
-      SOURCE_QUERY_FAMILY.includes(action)
-    );
-  }
-
-  private isSourceUpdate(
-    permission: PermissionNodes,
-    action?: string,
-  ): boolean {
-    return (
-      !!action &&
-      permission.type === 'source' &&
-      permission.action === 'update' &&
-      SOURCE_UPDATE_FAMILY.includes(action)
-    );
-  }
-
-  private createPermissions(): PermissionNodes[] {
-    return this.roles
-      .filter(this.filterRoles.bind(this))
-      .reduce(this.mergePermissions.bind(this), [])
-      .map((value) => new PermissionConvertor().parse(value));
-  }
-
-  private filterRoles(role: RoleSchema) {
-    return this.grantedRoles.includes(role.name);
-  }
-
-  private mergePermissions(permissions: string[], role: RoleSchema) {
-    return union(permissions, role.permissions);
+    return this.granter.grant(type, name, action);
   }
 }
