24f40513fe9d1897cbebff3031a623e953109e3b
Chase Wu <minowu@foxmail.com>
2023-01-24 13:41:48 +0800

    chore: #57 initialize integration testing.

---
+++ apps/server/integration-testing/tests/org.test.ts
@@ -0,0 +1,7 @@
+describe('org', () => {
+  describe('GET public/:orgName/org', () => {
+    it('When request a org api, then return basic information without secret information.', () => {
+      expect(1).toEqual(1);
+    });
+  });
+});
