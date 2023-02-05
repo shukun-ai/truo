34873ab5f5034d226624582a61ffb8114b2dedd6
Chase Wu <minowu@foxmail.com>
2023-02-05 20:34:21 +0800

    feat: #65 remove old migration from cli.

--- libs/cli/src/migration-generator/helpers/extract-shared.ts
+++
@@ -1,7 +0,0 @@
-export const IDString = 'IDString';
-export const DateTimeIsoString = 'DateTimeIsoString';
-export const AttachmentSchema = 'AttachmentSchema';
-
-export function extractShard(): string {
-  return ``;
-}
