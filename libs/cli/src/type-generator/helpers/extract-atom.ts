15af1e60befab7e13ad9395ae4332ae07d5fd126
Chase Wu <minowu@foxmail.com>
2023-02-07 15:38:44 +0800

    feat: #60 generate owner in cli.

--- libs/cli/src/type-generator/helpers/extract-atom.ts
+++ libs/cli/src/type-generator/helpers/extract-atom.ts
@@ -17,7 +17,7 @@ export function extractAtom(atom: MetadataSchema): string {
 
   text += `export interface ${toPascalCase(atom.name)}Model {`;
 
-  text += `_id: IDString; createdAt?: DateTimeIsoString; updatedAt?: DateTimeIsoString; `;
+  text += `_id: IDString; owner?: IDString; createdAt?: DateTimeIsoString; updatedAt?: DateTimeIsoString; `;
 
   atom.electrons.forEach((electron) => {
     text += extractElectron(electron);
