124beabc23e0101b799b6f931ad881fd94ba5016
Chase Wu <minowu@foxmail.com>
2023-02-09 13:42:35 +0800

    feat: #68 Make the low-code type generate type instead of interface.

--- libs/cli/src/index.ts
+++ libs/cli/src/index.ts
@@ -1,6 +1,4 @@
-export * from './io/read-application';
-export * from './io/write-file';
-
 export * from './generate/generate';
+
 export * from './type-generator/type-generator';
 export * from './type-generator/type.generator.type';
--- libs/cli/src/type-generator/helpers/extract-shared.ts
+++ libs/cli/src/type-generator/helpers/extract-shared.ts
@@ -3,5 +3,5 @@ export const DateTimeIsoString = 'DateTimeIsoString';
 export const AttachmentSchema = 'AttachmentSchema';
 
 export function extractShard(): string {
-  return `import { ${AttachmentSchema} } from "@shukun/schema"; export type ${IDString} = string; export type ${DateTimeIsoString} = string;`;
+  return `import { ${AttachmentSchema} } from "@shukun/schema";\n export type ${IDString} = string;\n export type ${DateTimeIsoString} = string;\n`;
 }
