aedef3f5d5c0a3b35f70fcc07bf0b8cd06264d81
Chase Wu <minowu@foxmail.com>
2023-02-12 18:58:10 +0800

    feat: #75 remove unused files.

--- apps/player/src/interface/store-container.interface.ts
+++
@@ -1,5 +0,0 @@
-import { Subscription } from 'rxjs';
-
-export interface IStoreContainer {
-  nihao?: string;
-}
--- apps/player/src/interface/store.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Subscription } from 'rxjs';
-
-export interface IStore {
-  register(storeName: string): void;
-  subscribe(expression: string): Subscription;
-}
