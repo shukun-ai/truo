5466f7338a2c4191ff89c0f4814d80fc37f8230e
Chase Wu <minowu@foxmail.com>
2023-02-20 14:17:55 +0800

    feat: add data source upload.

--- apps/client/src/pages/upload/Upload.tsx
+++ apps/client/src/pages/upload/Upload.tsx
@@ -1,62 +1,21 @@
-import { makeStyles } from '@material-ui/styles';
-import { Card, Spin } from 'antd';
-import React, { FunctionComponent, useCallback, useState } from 'react';
-import { useDropzone } from 'react-dropzone';
-import { BsFillFileCodeFill } from 'react-icons/bs';
-
-import { developerRequester } from '../../apis/requester';
+import React, { FunctionComponent } from 'react';
 
 import { FluidLayout } from '../layout/FluidLayout';
+
+import { UploadCodebase } from './UploadCodebase';
+import { UploadDataSource } from './UploadDataSource';
 
 export interface UploadProps {}
 
 export const Upload: FunctionComponent<UploadProps> = () => {
-  const classes = useStyles();
-
-  const [loading, setLoading] = useState(false);
-
-  // TODO if we want to extract more functions, we must create a new store to save loading.
-  const onDrop = useCallback(async (acceptedFiles: File[]) => {
-    if (acceptedFiles.length > 0) {
-      setLoading(true);
-      const formData = new FormData();
-      formData.append('file', acceptedFiles[0]);
-      try {
-        await developerRequester.updateCodebase(formData);
-      } finally {
-        setLoading(false);
-      }
-    }
-  }, []);
-
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
-
   return (
     <FluidLayout>
-      <Card title="Upload your application JSON Schema" bordered={false}>
-        <Spin spinning={loading}>
-          <div className={classes.box} {...getRootProps()}>
-            <input {...getInputProps()} />
-            <BsFillFileCodeFill size="3em" style={{ marginRight: 16 }} />
-            {isDragActive
-              ? 'Drop the files here ...'
-              : 'Drag drop some files here, or click to select files'}
-          </div>
-        </Spin>
-      </Card>
+      <div style={{ marginBottom: 12 }}>
+        <UploadCodebase />
+      </div>
+      <div>
+        <UploadDataSource />
+      </div>
     </FluidLayout>
   );
 };
-
-const useStyles = makeStyles(() => ({
-  box: {
-    border: 'dashed 1px #ccc',
-    background: '#f2f2f2',
-    padding: 16,
-    borderRadius: 8,
-    height: 100,
-    display: 'flex',
-    alignItems: 'center',
-    cursor: 'pointer',
-  },
-}));
