574ad4481d7ba99a98d2472d5b09c6bb2dcb5bb8
Chase Wu <minowu@foxmail.com>
2023-02-21 14:14:45 +0800

    feat: #73 remove old template files.

--- apps/player/src/template/template.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Literal } from './template-literal.interface';
-
-export interface ITemplateService {
-  parse(template: string): Literal;
-  evaluate(literal: Literal, executedCodes: unknown[]): unknown;
-  execute(code: Literal['codes'][number], imports: unknown[]): unknown;
-}
