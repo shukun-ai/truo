1b746e64cf0959e5650670577b2b31fae0833f94
Chase Wu <minowu@foxmail.com>
2023-03-09 16:55:56 +0800

    feat: #73 new widget-react library.

---
+++ libs/widget-react/src/lib/widget-react.tsx
@@ -0,0 +1,14 @@
+import './widget-react.module.css';
+
+/* eslint-disable-next-line */
+export interface WidgetReactProps {}
+
+export function WidgetReact(props: WidgetReactProps) {
+  return (
+    <div>
+      <h1>Welcome to WidgetReact!</h1>
+    </div>
+  );
+}
+
+export default WidgetReact;
