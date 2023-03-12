952443c09a3f16068004f421c293b122c06e726a
Chase Wu <minowu@foxmail.com>
2023-03-12 16:36:05 +0800

    chore: #73 remove unused files.

--- apps/presenter/src/effects/integrate-testing/widgets/button.widget.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export type ButtonWidgetProps = {
-  text: string;
-};
-
-export const ButtonWidget = ({ text }: ButtonWidgetProps) => {
-  return <button>{text}</button>;
-};
--- apps/presenter/src/effects/integrate-testing/widgets/code.widget.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export type CodeWidgetProps = {
-  value: string;
-};
-
-export const CodeWidget = ({ value }: CodeWidgetProps) => {
-  return <div>{JSON.stringify(value)}</div>;
-};
