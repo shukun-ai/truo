62febd637b89c666f9f5a6024c8c056933a20ad9
Chase Wu <minowu@foxmail.com>
2023-03-18 17:50:14 +0800

    feat: #73 new group

--- libs/widget/src/definitions/index.ts
+++ libs/widget/src/definitions/index.ts
@@ -2,10 +2,12 @@ import { WidgetSchema } from '@shukun/schema';
 
 import buttonDefinitionJson from './button.definition.json';
 import codeDefinitionJson from './code.definition.json';
+import groupDefinitionJson from './group.definition.json';
 import inputDefinitionJson from './input.definition.json';
 import textDefinitionJson from './text.definition.json';
 
 export const buttonDefinition = buttonDefinitionJson as WidgetSchema;
 export const codeDefinition = codeDefinitionJson as WidgetSchema;
+export const groupDefinition = groupDefinitionJson as WidgetSchema;
 export const inputDefinition = inputDefinitionJson as WidgetSchema;
 export const textDefinition = textDefinitionJson as WidgetSchema;
