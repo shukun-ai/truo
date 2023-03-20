2fed7e0635cb5ca7907a0366935a92203b0eab6a
Chase Wu <minowu@foxmail.com>
2023-03-20 17:12:59 +0800

    feat: #73 update input widget.

--- libs/widget-react/src/widgets/input/input.widget.tsx
+++ libs/widget-react/src/widgets/input/input.widget.tsx
@@ -1,3 +1,4 @@
+import { FormControl, OutlinedInput, FormHelperText } from '@mui/material';
 import { inputDefinition, InputDefinitionProps } from '@shukun/widget';
 
 import { createWidget } from '../../abstracts/create-widget';
@@ -6,20 +7,41 @@ export const InputWidget = createWidget<InputDefinitionProps>(
   inputDefinition,
   (props) => {
     return (
-      <div>
-        <div>
-          <div>{props.label}</div>
-          <div>{props.labelCaption}</div>
-        </div>
-        <div>
-          <input
-            value={props.value}
-            onChange={(event) =>
-              props.change && props.change(event.target.value)
-            }
-          />
-        </div>
-      </div>
+      <FormControl fullWidth>
+        {!props.labelHidden && (
+          <label>
+            {props.label}
+            {props.labelCaption}
+          </label>
+        )}
+
+        <OutlinedInput
+          type={props.type}
+          value={props.value}
+          multiline={props.multiline}
+          placeholder={props.placeholder}
+          disabled={props.disabled}
+          readOnly={props.readOnly}
+          fullWidth
+          onChange={(event) => props.change && props.change(event.target.value)}
+        />
+        <FormHelperText id="my-helper-text">{props.helper}</FormHelperText>
+      </FormControl>
     );
   },
 );
+
+// <div>
+//         <div>
+//           <div>{props.label}</div>
+//           <div>{props.labelCaption}</div>
+//         </div>
+//         <div>
+//           <input
+//             value={props.value}
+//             onChange={(event) =>
+//               props.change && props.change(event.target.value)
+//             }
+//           />
+//         </div>
+//       </div>
