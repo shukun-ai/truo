ed5445ac9504db4b56d1bb5d902b809926770383
Chase Wu <minowu@foxmail.com>
2023-03-20 14:01:55 +0800

    feat: #73 remove legacy theme.

--- apps/presenter/src/effects/browser-effect-injector.ts
+++ apps/presenter/src/effects/browser-effect-injector.ts
@@ -8,41 +8,28 @@ import { ServerLoader } from './loaders/server-loader';
 import { CurrentUserRepository } from './repositories/current-user-repository';
 import { RouterRepository } from './repositories/router-repository';
 import { SimpleRepository } from './repositories/simple-repository';
-import { ThemeRepository } from './repositories/theme-repository';
 import { RepositoryManager } from './repository/repository-manager';
 import { IRepositoryManager } from './repository/repository-manager.interface';
 import { AuthStorage } from './storages/auth-storage';
-import { registerReboot } from './styles/register-reboot-styles';
 import { TemplateService } from './template/template-service';
 
 export const createBrowserEffect = async () => {
-  registerReboot();
   const authStorage = new AuthStorage();
   const apiRequester = new ApiRequester(authStorage);
   const history = createBrowserHistory();
   const routerRepository = new RouterRepository(history);
   const loader = new ServerLoader(apiRequester);
-  const themeRepository = new ThemeRepository();
   const templateService = new TemplateService();
   const currentUserRepository = new CurrentUserRepository();
   const repositoryManager = new RepositoryManager();
   const eventQueue = new EventQueue(repositoryManager);
 
-  const THEME_REPOSITORY_KEY = 'theme';
   const CURRENT_USER_REPOSITORY_KEY = 'currentUser';
   const ROUTER_REPOSITORY_KEY = 'router';
 
   const router = routerRepository.getValue();
   const definitions = await loader.load(router.orgName, router.app);
 
-  repositoryManager.register(
-    {
-      scope: 'app',
-      containerId: 'app',
-      repositoryId: THEME_REPOSITORY_KEY,
-    },
-    themeRepository,
-  );
   repositoryManager.register(
     {
       scope: 'app',
