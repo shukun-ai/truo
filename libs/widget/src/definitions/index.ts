a2cbbda5d875df6c67b1408fbaa5ada38062ec46
Chase Wu <minowu@foxmail.com>
2023-03-26 13:17:14 +0800

    feat: remove unused widgets.

--- libs/widget/src/definitions/index.ts
+++ libs/widget/src/definitions/index.ts
@@ -2,33 +2,24 @@ import { WidgetSchema } from '@shukun/schema';
 
 import boxDefinitionJson from './box.definition.json';
 import buttonDefinitionJson from './button.definition.json';
-import cardDefinitionJson from './card.definition.json';
 import codeDefinitionJson from './code.definition.json';
-import containerDefinitionJson from './container.definition.json';
 import formDefinitionJson from './form.definition.json';
 import gridDefinitionJson from './grid.definition.json';
-import groupDefinitionJson from './group.definition.json';
 import inputDefinitionJson from './input.definition.json';
 import listDefinitionJson from './list.definition.json';
 import navLinkDefinitionJson from './nav-link.definition.json';
 import selectDefinitionJson from './select.definition.json';
-import stackDefinitionJson from './stack.definition.json';
 import tableDefinitionJson from './table.definition.json';
 import textDefinitionJson from './text.definition.json';
 
 export const boxDefinition = boxDefinitionJson as any as WidgetSchema;
 export const buttonDefinition = buttonDefinitionJson as any as WidgetSchema;
-export const cardDefinition = cardDefinitionJson as any as WidgetSchema;
 export const codeDefinition = codeDefinitionJson as any as WidgetSchema;
-export const containerDefinition =
-  containerDefinitionJson as any as WidgetSchema;
 export const formDefinition = formDefinitionJson as any as WidgetSchema;
 export const gridDefinition = gridDefinitionJson as any as WidgetSchema;
-export const groupDefinition = groupDefinitionJson as any as WidgetSchema;
 export const inputDefinition = inputDefinitionJson as any as WidgetSchema;
 export const listDefinition = listDefinitionJson as any as WidgetSchema;
 export const navLinkDefinition = navLinkDefinitionJson as any as WidgetSchema;
 export const selectDefinition = selectDefinitionJson as any as WidgetSchema;
-export const stackDefinition = stackDefinitionJson as any as WidgetSchema;
 export const tableDefinition = tableDefinitionJson as any as WidgetSchema;
 export const textDefinition = textDefinitionJson as any as WidgetSchema;
