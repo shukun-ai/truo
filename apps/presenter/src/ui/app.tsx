1fcf24a219ea417cd84683910f13534896e8606f
Chase Wu <minowu@foxmail.com>
2023-03-31 15:56:13 +0800

    feat: #82 new sign in page.

--- apps/presenter/src/ui/app.tsx
+++ apps/presenter/src/ui/app.tsx
@@ -3,12 +3,17 @@ import { AppProps } from '@shukun/widget-react';
 import { useMemo } from 'react';
 
 import { Screen } from './screen';
+import { SignInScreen } from './system/sign-in-screen';
 
 export const App = (props: AppProps) => {
   const currentScreen = useMemo<PresenterScreen | null>(() => {
     const screen = props.presenter.screens[props.context.screen];
     return screen ?? null;
   }, [props.presenter.screens, props.context.screen]);
+
+  if (props.context.screen === 'sign-in') {
+    return <SignInScreen app={props} />;
+  }
 
   if (!currentScreen) {
     return <div>Did not found page.</div>;
---
+++ apps/presenter/src/ui/system/use-sign-in-form.tsx
@@ -0,0 +1,52 @@
+import { useForm } from '@mantine/form';
+import { AppProps } from '@shukun/widget-react';
+import { useState } from 'react';
+
+export const useSignInForm = (app: AppProps) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
+
+  const form = useForm<{ username: string; password: string }>({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validate: {
+      username: (value) => (value.length < 2 ? '用户名必须大于2位' : null),
+      password: (value) => (value.length < 6 ? '用户名必须大于6位' : null),
+    },
+  });
+
+  const handleSubmit = form.onSubmit(async (values) => {
+    try {
+      setLoading(true);
+      const response = await app.api.publicRequester.signIn(
+        app.context.orgName,
+        {
+          username: values.username,
+          password: values.password,
+        },
+      );
+    } catch (error) {
+      setErrorMessage(handleErrorMessage(error));
+    } finally {
+      setLoading(false);
+    }
+  });
+
+  return { form, handleSubmit, errorMessage, loading };
+};
+
+const handleErrorMessage = (error: any): string => {
+  const message = error?.response?.data?.message;
+  if (typeof message === 'string') {
+    return message;
+  } else if (Array.isArray(message)) {
+    return message.join(', ');
+  } else if (error?.message) {
+    return error.message;
+  } else {
+    console.error(error);
+    return '发生异常错误';
+  }
+};
