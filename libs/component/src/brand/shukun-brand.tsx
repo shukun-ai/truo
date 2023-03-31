875c7f63ec8b3542b8df037b7dba8a1f2128d441
Chase Wu <minowu@foxmail.com>
2023-03-31 15:55:41 +0800

    feat: extract brand into compoennt.

---
+++ libs/component/src/brand/shukun-brand.tsx
@@ -0,0 +1,46 @@
+export type ShukunBrandProps = {
+  theme?: 'light' | 'dark';
+  title?: string;
+};
+
+export const ShukunBrand = ({
+  theme,
+  title = 'Shukun System',
+}: ShukunBrandProps) => {
+  return (
+    <div style={{ display: 'flex' }}>
+      <div style={{ paddingTop: 4, marginRight: 8 }}>
+        <div
+          style={{
+            display: 'inline-block',
+            width: 20,
+            height: 20,
+            background: 'rgba(55, 169, 92, 1)',
+            borderRadius: 10,
+          }}
+        ></div>
+        <div
+          style={{
+            display: 'inline-block',
+            width: 20,
+            height: 20,
+            background: 'rgba(28, 82, 108, 1)',
+            borderRadius: 10,
+            marginLeft: -8,
+          }}
+        ></div>
+      </div>
+      <div
+        style={{
+          borderRadius: 8,
+          color: theme === 'dark' ? 'rgba(255,255,255,0.9)' : 'rgba(0,0,0,0.8)',
+          fontSize: 14,
+
+          paddingTop: 4,
+        }}
+      >
+        {title}
+      </div>
+    </div>
+  );
+};
--- libs/component/src/index.ts
+++ libs/component/src/index.ts
@@ -1,1 +1,3 @@
 export * from './utils/extract-form-item';
+export * from './brand/shukun-brand';
+export * from './brand/org-brand';
