0a82155abd9e4c846a8642716f76d976135ed519
Chase Wu <minowu@foxmail.com>
2023-04-08 09:39:28 +0800

    chore:

--- apps/server/src/util/query/interfaces.ts
+++ apps/server/src/util/query/interfaces.ts
@@ -10,31 +10,3 @@ export interface QueryResponse<ValueType> {
  * @deprecated
  */
 export type QueryParserOptions = QueryOptions & { count?: boolean };
-
-/**
- * @deprecated
- */
-export type QueryFilter = Record<
-  string,
-  | string
-  | number
-  | boolean
-  | {
-      $eq?: string | number | boolean;
-      $ne?: string | number | boolean;
-      $gt?: string | number;
-      $gte?: string | number;
-      $lt?: string | number;
-      $lte?: string | number;
-      $foreign?: {
-        filter: QueryFilter;
-      } & Omit<QueryParserOptions, 'filter'>;
-      $in?: (string | number)[];
-      $nin?: (string | number)[];
-      $exists?: boolean;
-      $regex?: string;
-      $options?: string;
-      $and?: QueryFilter[];
-      $or?: QueryFilter[];
-    }
->;
