ef11393535b6c81cfef670779eeaf31209eade1a
Chase Wu <minowu@foxmail.com>
2023-05-18 19:46:10 +0800

    feat: use globalRepository instead of dashboardRepository

--- apps/editor2/src/app/contexts/app-context.tsx
+++ apps/editor2/src/app/contexts/app-context.tsx
@@ -3,24 +3,25 @@ import { createContext, useContext } from 'react';
 import { ApiRequester } from '../../apis/requester';
 import { AuthRepository } from '../../repositories/auth-repository';
 import { authStore } from '../../repositories/auth-store';
-import { DashboardRepository } from '../../repositories/dashboard-repository';
+import { GlobalRepository } from '../../repositories/global-repository';
+import { globalStore } from '../../repositories/global-store';
 
 export type AppContextProps = {
   repositories: {
     authRepository: AuthRepository;
-    dashboardRepository: DashboardRepository;
+    globalRepository: GlobalRepository;
   };
 };
 
 export const initializeAppContextProps = (): AppContextProps => {
   const apiRequester = new ApiRequester(authStore);
   const authRepository = new AuthRepository(authStore, apiRequester);
-  const dashboardRepository = new DashboardRepository(apiRequester);
+  const globalRepository = new GlobalRepository(globalStore, apiRequester);
 
   return {
     repositories: {
       authRepository,
-      dashboardRepository,
+      globalRepository,
     },
   };
 };
