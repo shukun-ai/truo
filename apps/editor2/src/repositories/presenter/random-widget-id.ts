6d7142462e1d49f17a82648988d2de031d560d7b
Chase Wu <minowu@foxmail.com>
2023-05-24 16:19:31 +0800

    feat: update create

--- apps/editor2/src/repositories/presenter/random-widget-id.ts
+++ apps/editor2/src/repositories/presenter/random-widget-id.ts
@@ -4,6 +4,11 @@ const alphabet =
   '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 const nanoid = customAlphabet(alphabet, 6);
 
-export const createRandomWidgetId = () => {
-  return nanoid();
+export const createRandomWidgetId = (existIds: string[]): string => {
+  const newId = nanoid();
+  if (existIds.includes(newId)) {
+    return createRandomWidgetId(existIds);
+  } else {
+    return newId;
+  }
 };
