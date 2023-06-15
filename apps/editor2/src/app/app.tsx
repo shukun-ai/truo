92b79b925ed92af1e348ba80b64dfd59cdeb6534
Chase Wu <minowu@foxmail.com>
2023-06-15 13:21:42 +0800

    chore: update @mantine and add notications

--- apps/editor2/src/app/app.tsx
+++ apps/editor2/src/app/app.tsx
@@ -1,4 +1,5 @@
 import { ModalsProvider } from '@mantine/modals';
+import { Notifications } from '@mantine/notifications';
 import { StrictMode } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ export const App = () => {
       <AppProvider>
         <ThemeProvider>
           <ModalsProvider>
+            <Notifications position="top-center" />
             <RouterProvider router={router} />
           </ModalsProvider>
         </ThemeProvider>
