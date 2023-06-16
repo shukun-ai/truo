21276a588df629177aa06d916f3a25946c2c1ba4
Chase Wu <chase_wu@shukun.work>
2023-06-16 17:00:58 +0800

    fix: unhandlerejection error catch from requester axios (#95)

--- apps/client/src/utils/exceptions/registerExceptionListeners.ts
+++ apps/client/src/utils/exceptions/registerExceptionListeners.ts
@@ -1,3 +1,4 @@
+import { ApiResponseException } from '@shukun/api';
 import { message } from 'antd';
 
 import { sessionService } from '../../services/session';
@@ -18,22 +19,24 @@ export function registerExceptionListeners() {
   );
 }
 
-function handleHttpException(error: any) {
-  const statusCode: number = error?.response?.status;
-
-  if (statusCode === 401) {
-    const orgName = sessionService.getOrgName();
-    if (orgName === null) {
-      history.push(RoutePath.Hub);
-    } else {
-      history.push(replaceOrgPath(RoutePath.SignIn, orgName));
+function handleHttpException(error: unknown) {
+  if (error instanceof ApiResponseException) {
+    if (error.status === 401) {
+      const orgName = sessionService.getOrgName();
+      if (orgName === null) {
+        history.push(RoutePath.Hub);
+      } else {
+        history.push(replaceOrgPath(RoutePath.SignIn, orgName));
+      }
+      return;
     }
-    return;
+
+    if (error.status >= 400) {
+      const errorMessage = error.message || '网络请求异常';
+      message.error({ content: errorMessage, duration: 2000 });
+      return;
+    }
   }
 
-  if (statusCode >= 400) {
-    const errorMessage = error?.response?.data?.message || '网络请求异常';
-    message.error({ content: errorMessage, duration: 2000 });
-    return;
-  }
+  console.error(error);
 }
