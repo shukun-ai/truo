201afe2dfac23c4c22ad9127de0175dd6ede5ce5
Chase Wu <minowu@foxmail.com>
2023-06-29 15:32:11 +0800

    feat: add repository definition

---
+++ libs/widget/src/repositories/definitions/index.ts
@@ -0,0 +1,8 @@
+import flowRepositoryDefinitionJson from './flow.repository.json';
+import simpleRepositoryDefinitionJson from './simple.repository.json';
+import sourceQueryRepositoryDefinitionJson from './source-query.repository.json';
+
+export const flowRepositoryDefinition = flowRepositoryDefinitionJson;
+export const simpleRepositoryDefinition = simpleRepositoryDefinitionJson;
+export const sourceQueryRepositoryDefinition =
+  sourceQueryRepositoryDefinitionJson;
