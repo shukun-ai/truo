6f0fc1d21de932565b8f3c309afcfc02d4e192f4
Chase Wu <minowu@foxmail.com>
2023-07-08 12:53:06 +0800

    feat: new task project

---
+++ libs/connector/task/src/lib/connector-task.spec.ts
@@ -0,0 +1,7 @@
+import { connectorTask } from './connector-task';
+
+describe('connectorTask', () => {
+  it('should work', () => {
+    expect(connectorTask()).toEqual('connector-task');
+  });
+});
