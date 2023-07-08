10a01c8559fc11d97fe7625364f594d2965f5f4a
Chase Wu <minowu@foxmail.com>
2023-07-08 12:51:02 +0800

    chore: rename connect to connector

--- libs/connector/handler/jest.config.ts
+++ libs/connector/handler/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'connect-handler',
+  displayName: 'connector-handler',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/connect/handler',
+  coverageDirectory: '../../../coverage/libs/connector/handler',
 };
