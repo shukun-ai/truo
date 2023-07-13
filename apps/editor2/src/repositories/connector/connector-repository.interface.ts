2498c0d84fb17908c1e2467591473ad31da49d61
Chase Wu <minowu@foxmail.com>
2023-07-13 09:14:42 +0800

    fix: connector repository

--- apps/editor2/src/repositories/connector/connector-repository.interface.ts
+++ apps/editor2/src/repositories/connector/connector-repository.interface.ts
@@ -5,7 +5,7 @@ import { ConnectorEntity } from './connector-ref';
 export interface IConnectorRepository {
   all$: Observable<ConnectorEntity[]>;
 
-  initialize(presenterName: string): Promise<void>;
+  initialize(): Promise<void>;
   create(connectorName: string): void;
   update(entity: ConnectorEntity): void;
   remove(entityId: string): void;
