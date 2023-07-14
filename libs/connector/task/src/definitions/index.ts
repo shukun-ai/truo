a964ce1d4f944fae8039431aee953f62986b6501
Chase Wu <minowu@foxmail.com>
2023-07-14 15:50:36 +0800

    feat: simplify tasks

--- libs/connector/task/src/definitions/index.ts
+++ libs/connector/task/src/definitions/index.ts
@@ -1,12 +1,12 @@
 import { TaskSchema } from '@shukun/schema';
 
-import choiceTaskJson from './choice.task.json';
+import eitherTaskJson from './either.task.json';
 import parallelTaskJson from './parallel.task.json';
 import repeatTaskJson from './repeat.task.json';
 import sourceQueryTaskJson from './source-query.task.json';
 import transformerTaskJson from './transformer.task.json';
 
-export const choiceTask = choiceTaskJson as TaskSchema;
+export const eitherTask = eitherTaskJson as TaskSchema;
 export const parallelTask = parallelTaskJson as TaskSchema;
 export const repeatTask = repeatTaskJson as TaskSchema;
 export const transformerTask = transformerTaskJson as TaskSchema;
