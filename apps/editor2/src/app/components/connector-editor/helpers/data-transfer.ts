56fe7b3d9f519516ec6649063efcd576fdb7177b
Chase Wu <minowu@foxmail.com>
2023-07-14 16:17:16 +0800

    feat: update data-transfer

--- apps/editor2/src/app/components/connector-editor/helpers/data-transfer.ts
+++ apps/editor2/src/app/components/connector-editor/helpers/data-transfer.ts
@@ -1,4 +1,4 @@
-import { ConnectorSchema } from '@shukun/schema';
+import { ConnectorSchema, ConnectorTask } from '@shukun/schema';
 import { Edge, Node } from 'reactflow';
 
 export type EditorState = {
@@ -8,18 +8,120 @@ export type EditorState = {
 
 export const toEditorState = (connector: ConnectorSchema): EditorState => {
   const state: EditorState = {
-    nodes: [],
-    edges: [],
+    nodes: [...addInternalNode(), ...addNodes(connector)],
+    edges: [...addStartEdge(connector), ...addEdges(connector)],
   };
 
-  Object.entries(connector.tasks).forEach(([taskName, task]) => {
-    const node: Node = {
-      id: taskName,
+  return state;
+};
+
+const addInternalNode = (): Node[] => {
+  return [
+    {
+      id: '$$__start',
+      type: 'start',
       position: { x: 0, y: 0 },
-      data: {
-        task,
+      data: {},
+    },
+    {
+      id: '$$__end',
+      type: 'end',
+      position: { x: 0, y: 0 },
+      data: {},
+    },
+  ];
+};
+
+const addNodes = (connector: ConnectorSchema): Node[] => {
+  return Object.entries(connector.tasks).reduce((total, [taskName, task]) => {
+    return [
+      ...total,
+      {
+        id: taskName,
+        type: createTaskNode(task),
+        position: { x: 0, y: 0 },
+        data: {
+          task,
+        },
       },
-    };
-    state.nodes.push(node);
-  });
+    ];
+  }, [] as Node[]);
+};
+
+const createTaskNode = (task: ConnectorTask) => {
+  switch (task.type) {
+    case 'parallel':
+    case 'repeat':
+    case 'either':
+    case 'fail':
+      return task.type;
+    default:
+      return 'resource';
+  }
+};
+
+const addStartEdge = (connector: ConnectorSchema): Edge[] => {
+  return [
+    {
+      id: `$$__start>${connector.start}`,
+      type: 'start',
+      source: '$$__start',
+      target: connector.start,
+    },
+  ];
+};
+
+const addEdges = (connector: ConnectorSchema): Edge[] => {
+  return Object.entries(connector.tasks).reduce((total, [taskName, task]) => {
+    return [
+      ...total,
+      ...createNextEdges(taskName, task),
+      ...createEitherEdges(taskName, task),
+      ...createFailEdges(taskName, task),
+    ];
+  }, [] as Edge[]);
+};
+
+const createNextEdges = (taskName: string, task: ConnectorTask): Edge[] => {
+  if (task.next) {
+    return [
+      {
+        id: `${taskName}>${task.next}`,
+        type: 'next',
+        source: taskName,
+        target: task.next,
+      },
+    ];
+  } else {
+    return [
+      {
+        id: `${taskName}>$$__end`,
+        type: 'end',
+        source: taskName,
+        target: '$$__end',
+      },
+    ];
+  }
+};
+
+const createEitherEdges = (taskName: string, task: ConnectorTask): Edge[] => {
+  if (task.type !== 'either') {
+    return [];
+  }
+  const right = task.parameters.right;
+  if (typeof right !== 'string') {
+    return [];
+  }
+  return [
+    {
+      id: `${taskName}>${right}`,
+      type: 'eitherRight',
+      source: taskName,
+      target: right,
+    },
+  ];
+};
+
+const createFailEdges = (taskName: string, task: ConnectorTask): Edge[] => {
+  return [];
 };
