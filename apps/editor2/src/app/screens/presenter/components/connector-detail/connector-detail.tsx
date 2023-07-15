29948f8f799977eccf3bfa92a5bc417e7d9803ed
Chase Wu <minowu@foxmail.com>
2023-07-15 10:51:20 +0800

    feat: show form and visual in connector detail

--- apps/editor2/src/app/screens/presenter/components/connector-detail/connector-detail.tsx
+++ apps/editor2/src/app/screens/presenter/components/connector-detail/connector-detail.tsx
@@ -1,13 +1,10 @@
-import { Box, Button, Group, Text, useMantineTheme } from '@mantine/core';
+import { Box, Container } from '@mantine/core';
 
 import { useForm } from '@mantine/form';
 
-import { isEqual } from 'lodash';
-import { useCallback, useEffect, useState } from 'react';
-
 import { ConnectorEntity } from '../../../../../repositories/connector/connector-ref';
 import { TabEntity } from '../../../../../repositories/tab/tab-ref';
-import { useAppContext } from '../../../../contexts/app-context';
+import { ConnectorEditor } from '../../../../components/connector-editor/connector-editor';
 
 import { Schema } from './internal/schema';
 
@@ -20,71 +17,32 @@ export const ConnectorDetail = ({
   tab,
   connectorEntity,
 }: ConnectorDetailProps) => {
-  const theme = useMantineTheme();
-
-  const app = useAppContext();
-
   const form = useForm<ConnectorEntity>({
     initialValues: structuredClone(connectorEntity),
   });
 
-  const [fixedCache, setFixedCache] = useState(false);
-
-  useEffect(() => {
-    if (!isEqual(form.values, connectorEntity) && !fixedCache) {
-      app.repositories.tabRepository.fixTab(tab.id);
-      app.repositories.tabRepository.activeEditTab(tab.id);
-      setFixedCache(true);
-    }
-  }, [
-    app.repositories.tabRepository,
-    connectorEntity,
-    fixedCache,
-    form.values,
-    tab.id,
-  ]);
-
-  const handleSubmit = useCallback(() => {
-    app.repositories.connectorRepository.update(form.values);
-    app.repositories.tabRepository.inactiveEditTab(tab.id);
-    setFixedCache(false);
-  }, [
-    app.repositories.connectorRepository,
-    app.repositories.tabRepository,
-    form.values,
-    tab.id,
-  ]);
-
   return (
-    <Box>
-      <form>
-        {tab.isEdit ? (
-          <Box
-            sx={{
-              background: theme.colors.blue[2],
-              marginLeft: -16,
-              marginRight: -16,
-              padding: 6,
-              paddingLeft: 16,
-              paddingRight: 16,
-              marginBottom: 16,
-              position: 'sticky',
-              top: 0,
-              zIndex: 1,
-            }}
-          >
-            <Group spacing="xs">
-              <Text>函数流正处于编辑状态，编辑后进行保存。</Text>
-              <Button size="xs" variant="light" onClick={handleSubmit}>
-                点击保存
-              </Button>
-            </Group>
-          </Box>
-        ) : (
-          <Box sx={{ height: 16 }}></Box>
-        )}
-        <Schema form={form} />
-      </form>
+    <Box
+      sx={{
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden',
+      }}
+    >
+      <Box sx={{ flex: 1, height: '100%' }}>
+        <ConnectorEditor
+          value={form.values}
+          onChange={() => {}}
+        ></ConnectorEditor>
+      </Box>
+      <Box sx={{ flex: 1, height: '100%' }}>
+        <Container fluid>
+          <form>
+            <Schema form={form} />
+          </form>
+        </Container>
+      </Box>
     </Box>
   );
 };
