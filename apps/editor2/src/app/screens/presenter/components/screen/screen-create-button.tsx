cc0a019ff89f92e9e8a38cd4907dee464673f292
Chase Wu <minowu@foxmail.com>
2023-07-19 21:17:25 +0800

    feat: change screen name

--- apps/editor2/src/app/screens/presenter/components/screen/screen-create-button.tsx
+++ apps/editor2/src/app/screens/presenter/components/screen/screen-create-button.tsx
@@ -29,8 +29,8 @@ export const ScreenCreateButton = () => {
         modals.closeAll();
       } catch {
         notifications.show({
-          title: '路由标识符重复',
-          message: '该路由标识符已存在，建议换个名称再次保存',
+          title: '页面标识符重复',
+          message: '该页面标识符已存在，建议换个名称再次保存',
           color: 'red',
         });
       }
@@ -40,7 +40,7 @@ export const ScreenCreateButton = () => {
 
   const open = useCallback(() => {
     modals.open({
-      title: '新建路由',
+      title: '新建页面',
       children: (
         <ScreenForm
           presenterName={presenterName}
@@ -59,7 +59,7 @@ export const ScreenCreateButton = () => {
       onClick={open}
       fullWidth
     >
-      新建路由
+      新建页面
     </Button>
   );
 };
