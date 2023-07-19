e4888951612676df81d114ac194789437ab11c90
Chase Wu <minowu@foxmail.com>
2023-07-19 08:29:18 +0800

    feat: add util-functions

---
+++ libs/util-functions/src/lib/util-functions.spec.ts
@@ -0,0 +1,7 @@
+import { utilFunctions } from './util-functions';
+
+describe('utilFunctions', () => {
+  it('should work', () => {
+    expect(utilFunctions()).toEqual('util-functions');
+  });
+});
---
+++ libs/util-functions/src/lib/util-functions.ts
@@ -0,0 +1,3 @@
+export function utilFunctions(): string {
+  return 'util-functions';
+}
