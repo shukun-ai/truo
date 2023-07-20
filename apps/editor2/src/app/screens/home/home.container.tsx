025eab5415f726d074de7a2197fb206cc7c278f5
Chase Wu <minowu@foxmail.com>
2023-07-20 21:30:21 +0800

    feat: add hub and org name login

--- apps/editor2/src/app/screens/home/home.container.tsx
+++ apps/editor2/src/app/screens/home/home.container.tsx
@@ -1,7 +1,9 @@
-import { Skeleton } from '@mantine/core';
 import { useObservableState } from 'observable-hooks';
-import { Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 
+import { Hub } from '../../components/hub/hub.container';
+import { OrgForm } from '../../components/hub/internal/org-form';
 import { useAppContext } from '../../contexts/app-context';
 import { routerMap } from '../../router-map';
 
@@ -11,6 +13,11 @@ export type HomeContainerProps = {
 
 export const HomeContainer = () => {
   const app = useAppContext();
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const navigate = useNavigate();
+
   const currentUser = useObservableState(
     app.repositories.authRepository.currentUser$,
     null,
@@ -24,6 +31,24 @@ export const HomeContainer = () => {
       />
     );
   } else {
-    return <Skeleton />;
+    return (
+      <Hub>
+        <OrgForm
+          errorMessage={errorMessage}
+          onSubmit={async (value) => {
+            try {
+              await app.apiRequester.publicRequester.getOrg(value.orgName);
+              navigate(routerMap.dashboard.replace(':orgName', value.orgName));
+            } catch (error) {
+              if (error instanceof Error) {
+                setErrorMessage(error.message);
+              } else {
+                setErrorMessage('未知错误，请联系管理员');
+              }
+            }
+          }}
+        />
+      </Hub>
+    );
   }
 };
