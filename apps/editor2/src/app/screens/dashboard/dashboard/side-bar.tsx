24d92cb31f4c5c3d8db6f4ced95a032b0f077d37
Chase Wu <minowu@foxmail.com>
2023-07-21 21:29:44 +0800

    feat: extract menu

--- apps/editor2/src/app/screens/dashboard/dashboard/side-bar.tsx
+++ apps/editor2/src/app/screens/dashboard/dashboard/side-bar.tsx
@@ -1,102 +1,40 @@
 import { Badge, Box, NavLink, Text } from '@mantine/core';
-import {
-  IconAffiliate,
-  IconApiApp,
-  IconAtom,
-  IconBook,
-  IconCloud,
-  IconDatabase,
-  IconHome2,
-  IconUserCheck,
-  IconUsers,
-  IconVariable,
-} from '@tabler/icons-react';
-import { Link } from 'react-router-dom';
+import { Link, matchPath, useMatches } from 'react-router-dom';
 
-import { useRouteOrgName } from '../../../hooks/use-route-org-name';
-import { routerMap } from '../../../router-map';
+import { useMenu } from './use-menu';
 
 export const SideBar = () => {
-  const routeOrgName = useRouteOrgName();
+  const menu = useMenu();
+
+  const matches = useMatches();
 
   return (
     <Box>
-      <Text size="xs" c="gray" fw="bold">
-        程序
-      </Text>
-      <NavLink
-        label="前端应用"
-        icon={<IconHome2 size="1rem" stroke={1.5} />}
-        component={Link}
-        to={routerMap.dashboard.replace(':orgName', routeOrgName)}
-        active
-      />
-      <NavLink
-        label="服务器应用"
-        icon={<IconCloud size="1rem" stroke={1.5} />}
-        component={Link}
-        to={routerMap.dashboardSystem.replace(':orgName', routeOrgName)}
-      />
-      <Text size="xs" c="gray" fw="bold">
-        数据
-      </Text>
-      <NavLink
-        label="数据库"
-        icon={<IconDatabase size="1rem" stroke={1.5} />}
-        disabled
-        rightSection={<Badge>未开放</Badge>}
-      />
-      <NavLink
-        label="API 接口"
-        icon={<IconApiApp size="1rem" stroke={1.5} />}
-        disabled
-        rightSection={<Badge>未开放</Badge>}
-      />
-      <Text size="xs" c="gray" fw="bold">
-        变量
-      </Text>
-      <NavLink
-        label="环境变量"
-        icon={<IconVariable size="1rem" stroke={1.5} />}
-        disabled
-        rightSection={<Badge>未开放</Badge>}
-      />
-      <NavLink
-        label="连接器资源"
-        icon={<IconAtom size="1rem" stroke={1.5} />}
-        disabled
-        rightSection={<Badge>未开放</Badge>}
-      />
-      <Text size="xs" c="gray" fw="bold">
-        用户
-      </Text>
-      <NavLink
-        label="用户管理"
-        icon={<IconUsers size="1rem" stroke={1.5} />}
-        disabled
-        rightSection={<Badge>未开放</Badge>}
-      />
-      <NavLink
-        label="角色组"
-        icon={<IconUserCheck size="1rem" stroke={1.5} />}
-        disabled
-        rightSection={<Badge>未开放</Badge>}
-      />
-      <NavLink
-        label="部门"
-        icon={<IconAffiliate size="1rem" stroke={1.5} />}
-        disabled
-        rightSection={<Badge>未开放</Badge>}
-      />
-      <Text size="xs" c="gray" fw="bold">
-        帮助
-      </Text>
-      <NavLink
-        label="使用手册"
-        icon={<IconBook size="1rem" stroke={1.5} />}
-        disabled
-        rightSection={<Badge>未开放</Badge>}
-      />
+      {menu.map((menuItem) => (
+        <>
+          <Text size="xs" c="gray" fw="bold">
+            {menuItem.label}
+          </Text>
+          {menuItem.children.map((child) => (
+            <NavLink
+              label={child.label}
+              icon={child.icon}
+              component={Link}
+              to={child.path ?? ''}
+              active={
+                child.path
+                  ? !!matchPath(
+                      child.path,
+                      matches[matches.length - 1].pathname,
+                    )
+                  : false
+              }
+              disabled={child.disabled}
+              rightSection={child.disabled && <Badge>未开放</Badge>}
+            />
+          ))}
+        </>
+      ))}
     </Box>
   );
 };
---
+++ apps/editor2/src/app/screens/dashboard/dashboard/use-menu.tsx
@@ -0,0 +1,107 @@
+import {
+  IconAffiliate,
+  IconApiApp,
+  IconAtom,
+  IconBook,
+  IconCloud,
+  IconDatabase,
+  IconHome2,
+  IconUserCheck,
+  IconUsers,
+  IconVariable,
+} from '@tabler/icons-react';
+import { ReactNode } from 'react';
+
+import { useRouteOrgName } from '../../../hooks/use-route-org-name';
+import { routerMap } from '../../../router-map';
+
+export const useMenu = (): {
+  label: string;
+  children: {
+    label: string;
+    icon: ReactNode;
+    path?: string;
+    disabled?: boolean;
+  }[];
+}[] => {
+  const routeOrgName = useRouteOrgName();
+
+  return [
+    {
+      label: '程序',
+      children: [
+        {
+          label: '前端应用',
+          icon: <IconHome2 size="1rem" stroke={1.5} />,
+          path: routerMap.dashboard,
+        },
+        {
+          label: '服务器应用',
+          icon: <IconCloud size="1rem" stroke={1.5} />,
+          path: routerMap.dashboardSystem.replace(':orgName', routeOrgName),
+        },
+      ],
+    },
+    {
+      label: '数据',
+      children: [
+        {
+          label: '数据库',
+          icon: <IconDatabase size="1rem" stroke={1.5} />,
+          disabled: true,
+        },
+
+        {
+          label: 'API 接口',
+          icon: <IconApiApp size="1rem" stroke={1.5} />,
+          disabled: true,
+        },
+      ],
+    },
+    {
+      label: '变量',
+      children: [
+        {
+          label: '环境变量',
+          icon: <IconVariable size="1rem" stroke={1.5} />,
+          disabled: true,
+        },
+        {
+          label: '连接器资源',
+          icon: <IconAtom size="1rem" stroke={1.5} />,
+          disabled: true,
+        },
+      ],
+    },
+    {
+      label: '用户',
+      children: [
+        {
+          label: '用户管理',
+          icon: <IconUsers size="1rem" stroke={1.5} />,
+          disabled: true,
+        },
+        {
+          label: '角色组',
+          icon: <IconUserCheck size="1rem" stroke={1.5} />,
+          disabled: true,
+        },
+        {
+          label: '部门',
+          icon: <IconAffiliate size="1rem" stroke={1.5} />,
+          disabled: true,
+        },
+      ],
+    },
+    {
+      label: '帮助',
+      children: [
+        {
+          label: '使用手册',
+          icon: <IconBook size="1rem" stroke={1.5} />,
+          disabled: true,
+        },
+      ],
+    },
+  ];
+};
