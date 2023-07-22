42cfc10cb24b5554eb9ae4ba8b1cb775830aead6
Chase Wu <minowu@foxmail.com>
2023-07-22 21:52:47 +0800

    feat: update metadata create tip

--- apps/editor2/src/app/screens/editor/components/metadata-detail/internal/create-electron-form.tsx
+++ apps/editor2/src/app/screens/editor/components/metadata-detail/internal/create-electron-form.tsx
@@ -1,5 +1,6 @@
 import { Button, TextInput } from '@mantine/core';
 import { useForm, zodResolver } from '@mantine/form';
+import { isElectronName } from '@shukun/validator';
 
 import { z } from 'zod';
 
@@ -22,9 +23,15 @@ export const CreateElectronForm = ({
       z.object({
         electronName: z
           .string()
-          .min(1, { message: '请输入任务名称后新建' })
-          .max(20, { message: '字段名称过长' }),
-        // TODO 与后台一致的验证
+          .min(1)
+          .max(20)
+          .refine(isElectronName, {
+            message: '请使用如下格式：大小写字母、数字，小写开头',
+          })
+          .refine(
+            (value) => !['owner', 'createdAt', 'updatedAt'].includes(value),
+            { message: 'owner, createdAt, updatedAt 为保留字段' },
+          ),
       }),
     ),
   });
@@ -37,10 +44,10 @@ export const CreateElectronForm = ({
     >
       <TextInput
         label="字段名称"
-        placeholder="Electron Name"
+        placeholder="大小写字母、数字，小写开头"
         data-autofocus
         withAsterisk
-        description="字段名用于字段识别，请使用符合如下格式：字母 a-z、数字 0-9、下划线。"
+        description="字段名用于字段识别，请使用如下格式：大小写字母、数字，小写开头"
         {...form.getInputProps('electronName')}
       />
       <Button type="submit" fullWidth mt="md">
