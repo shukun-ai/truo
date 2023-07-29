c7a8a207d766812d2abe5ddf51f77a3b1d4dfe95
Chase Wu <minowu@foxmail.com>
2023-07-29 10:43:53 +0800

    refactor: editor2 move to editor

--- apps/editor/vite.config.ts
+++ apps/editor/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/editor2',
+  cacheDir: '../../node_modules/.vite/editor',
 
   server: {
     port: 9020,
