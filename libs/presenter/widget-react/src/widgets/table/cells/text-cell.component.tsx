39d55be6f8f83b1d18e411256f2bbeaa7ee20b60
Chase Wu <minowu@foxmail.com>
2023-08-03 13:13:09 +0800

    refactor: remove old widgets

--- libs/presenter/widget-react/src/widgets/table/cells/text-cell.component.tsx
+++
@@ -1,7 +0,0 @@
-import { Text } from '@mantine/core';
-
-import { CellFactoryProps } from '../cell.interface';
-
-export const TextCell = ({ getValue }: CellFactoryProps) => {
-  return <Text>{getValue()}</Text>;
-};
