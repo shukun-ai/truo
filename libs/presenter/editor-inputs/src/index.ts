eba52ea6cd83362f1961f454b7db33977422302e
Chase Wu <minowu@foxmail.com>
2023-08-06 20:03:17 +0800

    feat: connector name

--- libs/presenter/editor-inputs/src/index.ts
+++ libs/presenter/editor-inputs/src/index.ts
@@ -4,3 +4,4 @@ export * from './number/number-input';
 export * from './boolean/boolean-input';
 export * from './data-binding/data-binding.input';
 export * from './box-model/box-model.input';
+export * from './connector-name/connector-name.input';
