dc1a467ceca2276d5f591379ef6d43ddd663f5d1
Chase Wu <minowu@foxmail.com>
2023-08-06 17:07:48 +0800

    refactor: refactor the repositories

--- apps/editor/src/widgets/repository-loader.ts
+++ apps/editor/src/widgets/repository-loader.ts
@@ -1,14 +1,5 @@
-import {
-  flowRepositoryDefinition,
-  routerRepositoryDefinition,
-  simpleRepositoryDefinition,
-  sourceQueryRepositoryDefinition,
-} from '@shukun/presenter/definition';
+import { editorRepositories } from '@shukun/presenter/definition';
 import { RepositorySchema } from '@shukun/schema';
 
-export const repositoryDefinitions: Record<string, RepositorySchema> = {
-  router: routerRepositoryDefinition,
-  simple: simpleRepositoryDefinition,
-  sourceQuery: sourceQueryRepositoryDefinition,
-  flow: flowRepositoryDefinition,
-};
+export const repositoryDefinitions: Record<string, RepositorySchema> =
+  editorRepositories;
