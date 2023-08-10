b168ce16a01949a99e2976207d92773fdd7f76da
Chase Wu <minowu@foxmail.com>
2023-08-10 13:14:17 +0800

    refactor: move dnd-provider

--- libs/component/src/index.ts
+++ libs/component/src/index.ts
@@ -4,3 +4,4 @@ export * from './brand/shukun-brand';
 export * from './brand/shukun-logo';
 export * from './brand/org-brand';
 export * from './array-inputs/array-inputs';
+export * from './dnd/dnd-provider';
