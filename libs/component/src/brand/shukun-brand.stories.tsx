57612b01cc8c6f1ad23270f61985021e64325ab0
Chase Wu <minowu@foxmail.com>
2023-08-10 13:14:33 +0800

    test: add two stories

--- libs/component/src/brand/shukun-brand.stories.tsx
+++ libs/component/src/brand/shukun-brand.stories.tsx
@@ -10,11 +10,6 @@ export default meta;
 
 type Story = StoryObj<typeof ShukunBrand>;
 
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/react/api/csf
- * to learn how to use render functions.
- */
 export const Primary: Story = {
   render: () => <ShukunBrand />,
 };
