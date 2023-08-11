2aa938b4bde29d535613d204ce5d2d5acbc85122
Chase Wu <minowu@foxmail.com>
2023-08-11 13:57:16 +0800

    feat: update styles

--- libs/component/src/array-inputs/sub/arrow-handler.tsx
+++ libs/component/src/array-inputs/sub/arrow-handler.tsx
@@ -1,6 +1,5 @@
-import { Stack, UnstyledButton } from '@mantine/core';
-import { IconArrowDown } from '@tabler/icons-react';
-import { IconArrowUp } from '@tabler/icons-react';
+import { Stack, Tooltip, UnstyledButton } from '@mantine/core';
+import { IconArrowDown, IconArrowUp } from '@tabler/icons-react';
 
 export type ArrowHandlerProps = {
   index: number;
@@ -10,22 +9,24 @@ export type ArrowHandlerProps = {
 export const ArrowHandler = ({ index, onMove }: ArrowHandlerProps) => {
   return (
     <Stack spacing={0}>
-      <UnstyledButton
-        onClick={() => {
-          //   onChange(move(value, index, index - 1));
-          onMove(index, index - 1);
-        }}
-      >
-        <IconArrowUp size="0.75rem" />
-      </UnstyledButton>
-      <UnstyledButton
-        onClick={() => {
-          //   onChange(move(value, index, index + 1));
-          onMove(index, index + 1);
-        }}
-      >
-        <IconArrowDown size="0.75rem" />
-      </UnstyledButton>
+      <Tooltip label="向上移动" withinPortal>
+        <UnstyledButton
+          onClick={() => {
+            onMove(index, index - 1);
+          }}
+        >
+          <IconArrowUp size="0.8rem" />
+        </UnstyledButton>
+      </Tooltip>
+      <Tooltip label="向下移动" withinPortal>
+        <UnstyledButton
+          onClick={() => {
+            onMove(index, index + 1);
+          }}
+        >
+          <IconArrowDown size="0.8rem" />
+        </UnstyledButton>
+      </Tooltip>
     </Stack>
   );
 };
