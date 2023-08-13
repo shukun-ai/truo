773c1facde6a5c3c882b276c506e90d848e3ed24
Chase Wu <minowu@foxmail.com>
2023-08-13 13:59:02 +0800

    feat: update AppProps

--- apps/presenter/src/interfaces/app.ts
+++ apps/presenter/src/interfaces/app.ts
@@ -1,10 +1,10 @@
-import { Injector } from '@shukun/presenter/definition';
+import { Injector, Repository } from '@shukun/presenter/definition';
 import { ReactWidget } from '@shukun/presenter/widget-react';
-import { PresenterSchema, RepositorySchema } from '@shukun/schema';
+import { PresenterSchema } from '@shukun/schema';
 
 export type AppProps = {
   injector: Injector;
   presenter: PresenterSchema;
   widgets: Record<string, ReactWidget>;
-  repositories: Record<string, RepositorySchema>;
+  repositories: Record<string, Repository>;
 };
