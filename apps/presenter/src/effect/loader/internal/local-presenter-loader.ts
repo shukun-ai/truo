0fe42593bc0b7b66a00df0dc10592b8c4eccdd4f
Chase Wu <minowu@foxmail.com>
2023-08-29 20:25:06 +0800

    feat: update local presenter loader

--- apps/presenter/src/effect/loader/internal/local-presenter-loader.ts
+++ apps/presenter/src/effect/loader/internal/local-presenter-loader.ts
@@ -1,11 +1,13 @@
 import { RouterState } from '@shukun/presenter/definition';
 import { PresenterSchema } from '@shukun/schema';
 
+import presenterJson from '../../../assets/presenter.json';
+
 export class LocalPresenterLoader {
   async load(router: RouterState): Promise<PresenterSchema> {
-    const response = await fetch('/presenter/assets/presenter.json');
-    const json = await response.json();
-
+    const json = presenterJson as unknown as PresenterSchema;
+    // eslint-disable-next-line no-console
+    console.log('local mode', json);
     return json;
   }
 }
