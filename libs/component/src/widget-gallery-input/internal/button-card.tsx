1155254edf588e4f5ea53fc88067268e41995d26
Chase Wu <minowu@foxmail.com>
2023-08-31 10:40:17 +0800

    feat: update widget-gallery-input

---
+++ libs/component/src/widget-gallery-input/internal/button-card.tsx
@@ -0,0 +1,86 @@
+import {
+  Box,
+  Popover,
+  Text,
+  UnstyledButton,
+  useMantineTheme,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { useMemo } from 'react';
+
+import {
+  ACTIVE_BG,
+  ACTIVE_COLOR,
+  DISABLED_BG,
+  DISABLED_COLOR,
+} from './color-scheme';
+
+export type ButtonCardProps = {
+  active: boolean;
+  disabled: boolean;
+  widgetDescription?: string;
+  onClick: () => void;
+  children: JSX.Element;
+};
+
+export const ButtonCard = ({
+  active,
+  disabled,
+  widgetDescription,
+  onClick,
+  children,
+}: ButtonCardProps) => {
+  const bg = useMemo(() => {
+    if (disabled) {
+      return DISABLED_BG;
+    } else if (active) {
+      return ACTIVE_BG;
+    } else {
+      return undefined;
+    }
+  }, [active, disabled]);
+
+  const color = useMemo(() => {
+    if (disabled) {
+      return DISABLED_COLOR;
+    } else if (active) {
+      return ACTIVE_COLOR;
+    } else {
+      return undefined;
+    }
+  }, [active, disabled]);
+
+  const theme = useMantineTheme();
+
+  const [opened, { close, open }] = useDisclosure(false);
+
+  return (
+    <Popover
+      width={200}
+      withArrow
+      withinPortal
+      shadow="md"
+      opened={opened}
+      disabled={widgetDescription || disabled ? false : true}
+    >
+      <Popover.Target>
+        <Box onMouseEnter={open} onMouseLeave={close}>
+          <UnstyledButton
+            bg={bg}
+            c={color}
+            onClick={onClick}
+            disabled={disabled}
+            sx={{ width: '100%', borderRadius: theme.radius.md, padding: 12 }}
+          >
+            {children}
+          </UnstyledButton>
+        </Box>
+      </Popover.Target>
+      <Popover.Dropdown sx={{ pointerEvents: 'none' }}>
+        <Text size="sm">
+          {disabled ? '该父组件下不支持创建该类型的子组件' : widgetDescription}
+        </Text>
+      </Popover.Dropdown>
+    </Popover>
+  );
+};
