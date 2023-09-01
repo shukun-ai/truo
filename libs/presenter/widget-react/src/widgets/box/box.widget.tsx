3715d589a45277e8d40b0b767a145940db717afc
Chase Wu <minowu@foxmail.com>
2023-09-01 17:03:05 +0800

    feat: add trim-box-model for '' value

--- libs/presenter/widget-react/src/widgets/box/box.widget.tsx
+++ libs/presenter/widget-react/src/widgets/box/box.widget.tsx
@@ -3,11 +3,12 @@ import { Box } from '@mantine/core';
 import { createWidget } from '../../abstracts/create-widget';
 
 import { BoxWidgetProps } from './box.props';
+import { trimBoxModel } from './internal/trim-box-model';
 
 export const BoxWidget = createWidget<BoxWidgetProps>(
   ({ children, boxModel, ...props }) => {
     return (
-      <Box {...boxModel} {...props}>
+      <Box {...trimBoxModel(boxModel)} {...props}>
         {children}
       </Box>
     );
