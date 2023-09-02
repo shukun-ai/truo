60724c80a3bd35b0d513eaa9330bb6004fce32be
Chase Wu <minowu@foxmail.com>
2023-09-02 09:24:02 +0800

    feat: add more supports for repository schema json

--- libs/presenter/widget-react/src/repositories/temporary/temporary.repository.ts
+++ libs/presenter/widget-react/src/repositories/temporary/temporary.repository.ts
@@ -1,6 +1,13 @@
 import { Repository } from '@shukun/presenter/definition';
 
 export const temporaryRepository: Repository = {
+  register(payload, event, injector, repository): void {
+    const defaultValue = repository.parameters['defaultValue'];
+    if (defaultValue) {
+      injector.store.update<any>([], () => defaultValue);
+    }
+  },
+
   setValue(payload, event, injector, repository): void {
     const { path } = event;
     const fullPath = path ? [event.target].concat(path) : [event.target];
