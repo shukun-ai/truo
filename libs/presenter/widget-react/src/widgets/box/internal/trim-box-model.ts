cc40d768834c3f4df989687c72a08824a95d3e4b
Chase Wu <minowu@foxmail.com>
2023-09-06 16:44:23 +0800

    fix: box model bug

--- libs/presenter/widget-react/src/widgets/box/internal/trim-box-model.ts
+++ libs/presenter/widget-react/src/widgets/box/internal/trim-box-model.ts
@@ -4,6 +4,9 @@ export const trimBoxModel = (
   boxModel: BoxWidgetProps['boxModel'],
 ): BoxWidgetProps['boxModel'] => {
   const newBoxModel: any = {};
+  if (!boxModel) {
+    return newBoxModel;
+  }
   for (const [name, value] of Object.entries(boxModel as any)) {
     newBoxModel[name] = value === '' ? undefined : value;
   }
