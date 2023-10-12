8c4d0fb9125be2207b44199d01f0c2472d3dc0c1
Chase Wu <minowu@foxmail.com>
2023-10-12 14:46:22 +0800

    refactor: remove unused files

--- libs/validator/src/schema-validator/internal/presenter-ajv.ts
+++ libs/validator/src/schema-validator/internal/presenter-ajv.ts
@@ -1,23 +1,13 @@
-import {
-  presenterContainerSchema,
-  presenterEventSchema,
-  presenterRepositorySchema,
-  presenterSchema,
-  presenterScreenSchema,
-  presenterWidgetSchema,
-} from '@shukun/schema';
+import { presenterSchema } from '@shukun/schema';
 import { Options } from 'ajv';
 
 import { createBaseAjv } from './base-ajv';
 
 export function createPresenterAjv(options?: Options) {
-  const validate = createBaseAjv(options)
-    .addSchema(presenterSchema, 'presenter.schema.json')
-    .addSchema(presenterScreenSchema, 'presenter/screen.schema.json')
-    .addSchema(presenterContainerSchema, 'presenter/container.schema.json')
-    .addSchema(presenterEventSchema, 'presenter/event.schema.json')
-    .addSchema(presenterRepositorySchema, 'presenter/repository.schema.json')
-    .addSchema(presenterWidgetSchema, 'presenter/widget.schema.json');
+  const validate = createBaseAjv(options).addSchema(
+    presenterSchema,
+    'presenter.schema.json',
+  );
 
   return validate;
 }
