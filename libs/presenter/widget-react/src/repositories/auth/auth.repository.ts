a017ebbc414628bcefbf6329111fa2d2f36e67f2
Chase Wu <minowu@foxmail.com>
2023-10-17 09:57:18 +0800

    refactor: remove unused repository

--- libs/presenter/widget-react/src/repositories/auth/auth.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from '@shukun/presenter/definition';
-
-export const authRepository: Repository = {
-  signOut: (payload, event, injector) => {
-    injector.auth.signOut();
-  },
-};
