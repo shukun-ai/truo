156bd02255fbcee37e9fbe01c0f9acdb061f16a6
Chase Wu <minowu@foxmail.com>
2023-10-18 12:22:17 +0800

    feat: new web-runtime

---
+++ libs/connector/web-runtime/src/lib/connector-web-runtime.spec.ts
@@ -0,0 +1,7 @@
+import { connectorWebRuntime } from './connector-web-runtime';
+
+describe('connectorWebRuntime', () => {
+  it('should work', () => {
+    expect(connectorWebRuntime()).toEqual('connector-web-runtime');
+  });
+});
---
+++ libs/connector/web-runtime/src/lib/connector-web-runtime.ts
@@ -0,0 +1,3 @@
+export function connectorWebRuntime(): string {
+  return 'connector-web-runtime';
+}
