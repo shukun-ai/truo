88a119f7742e984abe7f2ba36b0d67b566f01cd2
Chase Wu <minowu@foxmail.com>
2023-10-20 13:57:50 +0800

    feat: add web tasks

--- apps/editor/src/repositories/task/task-repository.ts
+++ apps/editor/src/repositories/task/task-repository.ts
@@ -1,6 +1,17 @@
 import { select } from '@ngneat/elf';
 import { setEntities } from '@ngneat/elf-entities';
 
+import {
+  assignTask,
+  eitherTask,
+  messageTask,
+  parallelTask,
+  repeatTask,
+  transformerTask,
+} from '@shukun/connector/task';
+
+import { TaskSchema } from '@shukun/schema';
+
 import { IApiRequester } from '../../apis/requester.interface';
 
 import { TaskEntity, createTaskEntityId, taskRef } from './task-ref';
@@ -10,8 +21,11 @@ export const taskRepository = {
   all$: taskStore.pipe(select((state) => state.taskEntities)),
 
   initialize: async (apiRequester: IApiRequester): Promise<void> => {
-    const response = await apiRequester.developerRequester.queryTask();
-    const entities: TaskEntity[] = Object.entries(response.data.value).map(
+    const loadWeb = true;
+    const tasks = loadWeb
+      ? await loadWebTasks(apiRequester)
+      : await loadNodeTasks(apiRequester);
+    const entities: TaskEntity[] = Object.entries(tasks).map(
       ([taskName, task]) => ({
         ...task,
         id: createTaskEntityId(taskName),
@@ -20,4 +34,24 @@ export const taskRepository = {
     );
     taskStore.update(setEntities(entities, { ref: taskRef }));
   },
+};
+
+const loadNodeTasks = async (
+  apiRequester: IApiRequester,
+): Promise<Record<string, TaskSchema>> => {
+  const response = await apiRequester.developerRequester.queryTask();
+  return response.data.value;
+};
+
+const loadWebTasks = async (
+  apiRequester: IApiRequester,
+): Promise<Record<string, TaskSchema>> => {
+  return {
+    either: eitherTask,
+    parallel: parallelTask,
+    repeat: repeatTask,
+    transformer: transformerTask,
+    message: messageTask,
+    assign: assignTask,
+  };
 };
