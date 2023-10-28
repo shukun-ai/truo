928256642dd13387d0904449f95295fada31ad7a
Chase Wu <minowu@foxmail.com>
2023-10-28 20:08:51 +0800

    refactor: remove repository from libs

--- libs/validator/src/schema-validators/repository/validate-repository-schema.spec.ts
+++
@@ -1,8 +0,0 @@
-import repositoryData from './repository.test.json';
-import { repositorySchemaValidator } from './validate-repository-schema';
-
-describe('repository format check.', () => {
-  it('repositorySchemaValidator', () => {
-    repositorySchemaValidator.validate(repositoryData);
-  });
-});
