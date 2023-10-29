77efe7bffc35342bb86c734fd7d1530dde43459c
Chase Wu <minowu@foxmail.com>
2023-10-29 10:41:20 +0800

    feat: update table widget

--- libs/presenter/widget-react/src/widgets/table/table.widget.tsx
+++ libs/presenter/widget-react/src/widgets/table/table.widget.tsx
@@ -1,9 +1,16 @@
 import { Table } from '@mantine/core';
 
-import { cloneElement, useMemo } from 'react';
+import { IconSortAscending, IconSortDescending } from '@tabler/icons-react';
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { useMemo } from 'react';
 
 import { createWidget } from '../../abstracts/create-widget';
 
+import { useColumns } from './internal/use-columns';
 import { useHeaders } from './internal/use-headers';
 import { TableWidgetProps } from './table.props';
 
@@ -14,28 +21,68 @@ export const TableWidget = createWidget<TableWidgetProps>(
     }, [data]);
 
     const headers = useHeaders(children);
+    const columns = useColumns(headers, children);
+
+    const table = useReactTable({
+      data: parsedData,
+      columns,
+      getCoreRowModel: getCoreRowModel(),
+      debugTable: false,
+    });
 
     return (
       <Table {...props}>
         <thead>
-          <tr>
-            {headers.map((header, index) => (
-              <td key={index}>{header.title}</td>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {parsedData.map((item, index) => (
-            <tr key={index}>
-              {children?.map((child, index) => {
-                return cloneElement(child, {
-                  item,
-                  index,
-                  key: index, // TODO get the key from child props
-                });
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <div
+                        {...{
+                          className: header.column.getCanSort()
+                            ? 'cursor-pointer select-none'
+                            : '',
+                          onClick: header.column.getToggleSortingHandler(),
+                        }}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                        {{
+                          asc: <IconSortAscending size={16} stroke={1.5} />,
+                          desc: <IconSortDescending size={16} stroke={1.5} />,
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
+                    )}
+                  </th>
+                );
               })}
             </tr>
           ))}
+        </thead>
+        <tbody>
+          {table
+            .getRowModel()
+            .rows.slice(0, 10)
+            .map((row) => {
+              return (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <td key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
         </tbody>
       </Table>
     );
