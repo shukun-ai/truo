d8ff36ca3497458fbca0cda94c8b30256dbbbf5a
Chase Wu <minowu@foxmail.com>
2023-11-12 19:54:12 +0800

    feat: add ribbon form

---
+++ libs/presenter/editor-ui/src/components/view-detail/internal/view-link-type-options.ts
@@ -0,0 +1,13 @@
+export const viewLinkTypeOptions = [
+  { label: '空按钮', value: 'None' },
+  // {label: "另一个视图", value: 'View', },
+  // {label: "", value: 'Webhook', },
+  { label: '新建', value: 'CreateOne' },
+  { label: '打开', value: 'ReadOne' },
+  { label: '更新', value: 'UpdateOne' },
+  { label: '删除', value: 'DeleteOne' },
+  { label: '打印', value: 'Print' },
+  { label: '导出 Excel', value: 'Excel' },
+  { label: '导出 CSV', value: 'Csv' },
+  { label: '打开自定义弹窗', value: 'CustomModal' },
+];
