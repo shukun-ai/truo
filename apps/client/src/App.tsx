babaa5583cb170f11e8cfe7e717b405dadbb10a3
Chase Wu <minowu@foxmail.com>
2024-04-26 14:44:48 +0800

    feat: add old browser support

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,3 +1,4 @@
+import { StyleProvider } from '@ant-design/cssinjs';
 import { LegacyFunctionComponent } from '@shukun/component';
 import { ConfigProvider } from 'antd';
 
@@ -7,21 +8,25 @@ export interface AppProps {}
 
 export const App: LegacyFunctionComponent<AppProps> = () => {
   return (
-    <ConfigProvider
-      autoInsertSpaceInButton={false}
-      theme={{
-        token: {
-          colorPrimary: '#4361ee',
-          colorInfo: '2196f3',
-          colorSuccess: '1abc9c',
-          colorError: '#e7515a',
-          colorHighlight: '#e7515a',
-          colorWarning: '#e2a03f',
-          borderRadius: 0,
-        },
-      }}
-    >
-      <Routes />
-    </ConfigProvider>
+    // add CSS Compatible for old browsers that doesn't support :where
+    // @see @link{https://ant.design/docs/react/compatible-style}
+    <StyleProvider hashPriority="high">
+      <ConfigProvider
+        autoInsertSpaceInButton={false}
+        theme={{
+          token: {
+            colorPrimary: '#4361ee',
+            colorInfo: '2196f3',
+            colorSuccess: '1abc9c',
+            colorError: '#e7515a',
+            colorHighlight: '#e7515a',
+            colorWarning: '#e2a03f',
+            borderRadius: 0,
+          },
+        }}
+      >
+        <Routes />
+      </ConfigProvider>
+    </StyleProvider>
   );
 };
