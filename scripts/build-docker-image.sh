7379f271ca4b969830a0e1f30516fce9dd70391a
Chase Wu <minowu@foxmail.com>
2024-06-08 13:56:00 +0800

    feat: update docker build

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -1,2 +1,3 @@
 cd ..
-DOCKER_BUILDKIT=1 docker build -t shukunai/platform:latest --platform=linux/amd64 --no-cache -f Dockerfile .
+DOCKER_BUILDKIT=1 docker build -t shukunai/platform:latest --target platform --platform=linux/amd64 --no-cache -f Dockerfile .
+DOCKER_BUILDKIT=1 docker build -t shukunai/hub:latest --target hub --platform=linux/amd64 --no-cache -f Dockerfile .
