35e964bf0724f4eb689326f8763299897023b470
Chase Wu <minowu@foxmail.com>
2024-06-09 21:46:04 +0800

    chore: change workflow

---
+++ build-docker-image.sh
@@ -0,0 +1,2 @@
+DOCKER_BUILDKIT=1 docker build -t shukunai/platform:latest --target platform --platform=linux/amd64 --no-cache -f Dockerfile .
+DOCKER_BUILDKIT=1 docker build -t shukunai/hub:latest --target hub --platform=linux/amd64 --no-cache -f Dockerfile .
--- scripts/build-web-client.sh
+++
@@ -1,7 +0,0 @@
-cd ..
-npx nx reset
-VITE_CLIENT_BASE_URL= VITE_CLIENT_STORAGE_URL= VITE_CLIENT_ASSET_URL= VITE_CLIENT_PUBLIC_PATH= npx nx run client:test
-VITE_CLIENT_BASE_URL= VITE_CLIENT_STORAGE_URL= VITE_CLIENT_ASSET_URL= VITE_CLIENT_PUBLIC_PATH= npx nx run client:build:production
-
-cd dist/apps
-zip -r client.zip client
