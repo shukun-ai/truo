60696362816e5e2c3e5feffb98e95f3653328400
Chase Wu <minowu@foxmail.com>
2024-08-24 15:26:28 +0800

    feat: update integrating test docker-compose version

--- apps/server/integration-testing/global-setup.js
+++ apps/server/integration-testing/global-setup.js
@@ -4,12 +4,15 @@ const dockerCompose = require('docker-compose');
 const isPortReachable = require('is-port-reachable');
 
 module.exports = async () => {
-  process.env.MONGO_URI = 'mongodb://localhost:20000/shukun_test';
+  const dockerComposeVersion = await dockerCompose.v2.version();
+  // eslint-disable-next-line no-console
+  console.log('dockerComposeVersion', dockerComposeVersion);
 
+  process.env.MONGO_URI = 'mongodb://localhost:20000/shukun_test';
   const isDBReachable = await isPortReachable(20000);
 
   if (!isDBReachable) {
-    await dockerCompose.upAll({
+    await dockerCompose.v2.upAll({
       cwd: path.join(__dirname),
       log: true,
       verbose: true,
