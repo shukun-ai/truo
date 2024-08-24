7da8f7fb2e86e2814a35dd861ebc23278403d4f1
Chase Wu <minowu@foxmail.com>
2024-08-24 14:28:40 +0800

    feat: update local environment

---
+++ apps/server/src/util/workflow/functions/cryptoPassword.spec.ts
@@ -0,0 +1,10 @@
+import { cryptoPassword } from './cryptoPassword';
+
+describe('cryptoPassword', () => {
+  it('cryptoPassword', () => {
+    const password = cryptoPassword('123456');
+    expect(password).toEqual(
+      '4ff1b5a2c4915baf130db8ab9690cd5bb32263911ec9f443a6b7df4604bc612453b419e6e9d1a78a921e468e5c60e5d536151c4d166c9a1ea46b5587ba1692a1',
+    );
+  });
+});
